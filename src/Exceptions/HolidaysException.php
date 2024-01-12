d3b4316e6196a3fd286184529bfbc83ee0534ac0
freekmurze <freekmurze@users.noreply.github.com>
2024-01-12 15:19:02 +0000

    Fix styling

--- src/Exceptions/HolidaysException.php
+++ src/Exceptions/HolidaysException.php
@@ -8,11 +8,11 @@ class HolidaysException extends RuntimeException
 {
     public static function yearTooLow(): self
     {
-        return new self("Holidays can only be calculated for years after 1970.");
+        return new self('Holidays can only be calculated for years after 1970.');
     }
 
     public static function yearTooHigh(): self
     {
-        return new self("Holidays can only be calculated for years before 2038.");
+        return new self('Holidays can only be calculated for years before 2038.');
     }
 }
