8beb0567d91a8867f048b29ca338fcdb74e3c043
Marlon Basten <marlon@gedankenburg.de>
2024-01-17 22:03:21 +0100

    added states and fixed NRW

--- tests/Countries/GermanyTest.php
+++ tests/Countries/GermanyTest.php
@@ -5,7 +5,7 @@ namespace Spatie\Holidays\Tests\Countries;
 use Carbon\CarbonImmutable;
 use Spatie\Holidays\Holidays;
 
-it('can calculate german holidays', function () {
+it('can calculate german national holidays', function () {
     CarbonImmutable::setTestNowAndTimezone('2024-01-01');
 
     $holidays = Holidays::for(country: 'de')->get();
@@ -16,3 +16,20 @@ it('can calculate german holidays', function () {
         ->and(formatDates($holidays))
         ->toMatchSnapshot();
 });
+
+it('can calculate german state-specific holidays', function (string $state) {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'de', state: $state)->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+})->with([
+    'bw',
+    'by',
+    'nw',
+]);
+
