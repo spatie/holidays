1c55b136fe4c0f9a549ed67f0fc4cab8d1bf2e51
Stephen S. Musoke <ssmusoke@gmail.com>
2024-01-17 21:44:53 +0300

    Added start dates for the celebration of new holidays added to the list

--- src/Countries/Uganda.php
+++ src/Countries/Uganda.php
@@ -17,13 +17,8 @@ class Uganda extends Country
     {
         return array_merge([
             'New Year\'s Day' => '01-01',
-            'NRM Liberation Day' => '01-26',
-            'Archbishop Janani Luwum Memorial Day' => '02-16',
             'International Women\'s Day' => '03-08',
             'Labor Day' => '05-01',
-            'Uganda Martyr\'s Day' => '06-03',
-            'National Heroes\' Day' => '06-09',
-            'Independence Day of Uganda' => '10-09',
             'Christmas Day' => '12-25',
             'Boxing Day' => '12-26'
         ], $this->variableHolidays($year));
@@ -55,11 +50,38 @@ class Uganda extends Country
         $eid_al_adha_cal->clear(IntlCalendar::FIELD_SECOND);
         $eid_al_adha_cal->clear(IntlCalendar::FIELD_MILLISECOND);
 
-        return [
+        $variable_holidays = array(
             'Easter Monday' => $easter->addDay(),
             'Good Friday' => $easter->addDays(-2),
             'Eid al-Fitr' => CarbonImmutable::createFromTimestamp($eid_al_fitr_cal->toDateTime()->getTimestamp()),
             'Eid al-Adha' => CarbonImmutable::createFromTimestamp($eid_al_adha_cal->toDateTime()->getTimestamp())
-        ];
+        );
+
+        // Independence day celebrations started in 1962
+        if ($year >= 1962) {
+            $variable_holidays['Independence Day of Uganda'] = '10-09';
+        }
+
+        // Martyr's day celebrations started in 1964
+        if ($year >= 1964) {
+           $variable_holidays['Uganda Martyr\'s Day'] = '06-03';
+        }
+
+        //  'NRM Liberation Day celebrations started in 1987
+        if ($year >= 1987) {
+            $variable_holidays['NRM Liberation Day'] = '01-26';
+        }
+
+        // National Heroes day celebration started in 2001
+        if ($year >= 2001) {
+            $variable_holidays['National Heroes\' Day'] = '06-09';
+        }
+
+        // Archbishop Janani Luwum Memorial Day celebrations started in 2015
+        if ($year >= 2015) {
+            $variable_holidays['Archbishop Janani Luwum Memorial Day'] = '02-16';
+        }
+
+        return $variable_holidays;
     }
 }
