cfb13c6351af068ec00c22b768d22aed15326ac5
Muhammad Nauman <naumancs@gmail.com>
2024-01-17 21:13:04 +0500

    added Pakistan as the country

---
+++ src/Countries/Pakistan.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Pakistan extends Country
+{
+    public function countryCode(): string
+    {
+        return 'pk';
+    }
+
+    /**
+     * @return array<string, CarbonImmutable>
+     * @link https://en.wikipedia.org/wiki/Public_holidays_in_Pakistan
+     */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Kashmir Solidarity Day' => '02-05',
+            'Pakistan Day' => '03-23',
+            'Labour Day' => '05-01',
+            'Independence Day' => '08-14',
+            'Iqbal Day' => '11-09',
+            'Quaid-e-Azam Day' => '12-25',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        // The variable holidays are all follow lunar calendar so their dates are not confirmed.
+        return [];
+    }
+}
