e729d130705af1ab2f77456de6758636bd4b577b
Stephen S. Musoke <ssmusoke@gmail.com>
2024-01-17 19:29:49 +0300

    Add computations for Eid al-Fitr and Eid al-Adha

--- src/Countries/Uganda.php
+++ src/Countries/Uganda.php
@@ -3,6 +3,7 @@
 namespace Spatie\Holidays\Countries;
 
 use Carbon\CarbonImmutable;
+use IntlCalendar;
 
 class Uganda extends Country
 {
@@ -34,9 +35,31 @@ class Uganda extends Country
         $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
             ->setTimezone('Africa/Kampala');
 
+        // Eid al-Fitr is on the first day of the 10th month in the calendar
+        // since the PHP array index begins at 0, it is the 9th month
+        $eid_al_fitr_cal = IntlCalendar::createInstance('Africa/Kampala', '@calendar=islamic-rgsa');
+        $eid_al_fitr_cal->set(IntlCalendar::FIELD_MONTH, 9);
+        $eid_al_fitr_cal->set(IntlCalendar::FIELD_DAY_OF_MONTH, 1);
+        $eid_al_fitr_cal->clear(IntlCalendar::FIELD_HOUR_OF_DAY);
+        $eid_al_fitr_cal->clear(IntlCalendar::FIELD_MINUTE);
+        $eid_al_fitr_cal->clear(IntlCalendar::FIELD_SECOND);
+        $eid_al_fitr_cal->clear(IntlCalendar::FIELD_MILLISECOND);
+
+        // Eid al-Adha is on the 10th day of the Dhu al-Hijja the 12th month in the calendar
+        // since the PHP array index begins at 0, it is the 11th month
+        $eid_al_adha_cal = IntlCalendar::createInstance('Africa/Kampala', '@calendar=islamic-civil');
+        $eid_al_adha_cal->set(IntlCalendar::FIELD_MONTH, 11);
+        $eid_al_adha_cal->set(IntlCalendar::FIELD_DAY_OF_MONTH, 10);
+        $eid_al_adha_cal->clear(IntlCalendar::FIELD_HOUR_OF_DAY);
+        $eid_al_adha_cal->clear(IntlCalendar::FIELD_MINUTE);
+        $eid_al_adha_cal->clear(IntlCalendar::FIELD_SECOND);
+        $eid_al_adha_cal->clear(IntlCalendar::FIELD_MILLISECOND);
+
         return [
             'Easter Monday' => $easter->addDay(),
             'Good Friday' => $easter->addDays(-2),
+            'Eid al-Fitr' => CarbonImmutable::createFromTimestamp($eid_al_fitr_cal->toDateTime()->getTimestamp()),
+            'Eid al-Adha' => CarbonImmutable::createFromTimestamp($eid_al_adha_cal->toDateTime()->getTimestamp())
         ];
     }
 }
