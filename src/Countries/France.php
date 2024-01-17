73e993b2775afe65830f97c4e34f735307ecc05f
levrailoup <wrmolf@gmail.com>
2024-01-17 15:55:02 +0100

    Fix France.php

--- src/Countries/France.php
+++ src/Countries/France.php
@@ -17,7 +17,7 @@ class France extends Country
         return array_merge([
             'Jour de l\'An' => '01-01',
             'Fête du Travail' => '05-01',
-            'Victoire 1945' => '05-08'
+            'Victoire 1945' => '05-08',
             'Fête Nationale' => '07-14',
             'Assomption' => '08-15',
             'Toussaint' => '11-01',
