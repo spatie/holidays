2c71a2cca2c608c87f56b5d0a85cdbf130d3505f
Dennis <hello@1nul.dev>
2024-01-17 14:42:45 +0100

    Added Danish holidays.

---
+++ src/Countries/Denmark.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Denmark extends Country
+{
+    public function countryCode(): string
+    {
+        return 'da';
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Nytår' => '01-01',
+            'Juleaften' => '24-12',
+            'Juledag' => '25-12',
+            'Anden Juledag' => '26-12'
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('Europe/Copenhagen');
+
+        return [
+            'Påskedag' => $easter->addDay(),
+            'Anden påskedag' => $easter->addDays(2),
+            'Kristi Himmelfartsdag' => $easter->addDays(39),
+            'Pinse' => $easter->addDays(49),
+            'Anden pisedag' => $easter->addDays(50),
+        ];
+    }
+}
