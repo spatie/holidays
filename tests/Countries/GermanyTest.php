1bc14c5dd6021d2b30e495b175949fbc65373047
Arne Breitsprecher <arnebr@gmail.com>
2024-01-17 15:09:51 +0100

    Create GermanyTest.php

---
+++ tests/Countries/GermanyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Spatie\Holidays\Tests\Countries;
+
+use Carbon\CarbonImmutable;
+use Spatie\Holidays\Holidays;
+
+it('can calculate german holidays', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'de')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+
+});
