b7bf6baf37521d0ac855ea0ca3879994d0b45a97
davsaniuv <58817543+davsaniuv@users.noreply.github.com>
2024-01-17 17:06:15 +0000

    Add Test

---
+++ tests/Countries/MexicoTest.php
@@ -0,0 +1,19 @@
+Me<?php
+
+namespace Spatie\Holidays\Tests\Countries;
+
+use Carbon\CarbonImmutable;
+use Spatie\Holidays\Holidays;
+
+it('can calculate dutch holidays', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'mx')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+
+});
