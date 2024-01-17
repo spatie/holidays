203b079fe500d5ea3cbee76cb90fd0e825f4f67e
yethusoe91 <>
2024-01-18 01:17:19 +0630

    test: update myanmar holidays

--- tests/Countries/MyanmarTest.php
+++ tests/Countries/MyanmarTest.php
@@ -7,11 +7,15 @@ use Spatie\Holidays\Holidays;
 
 it('can calculate myanmar holidays', function () {
 
+    $holidays = Holidays::for(country: 'mm')->get();
+
     $result = Holidays::for('mm')->getName(CarbonImmutable::parse('2024-12-25'));
 
     expect($result)->toBe('ခရစ်စမတ်နေ့');
 
-
     $result = Holidays::for('mm')->getName(CarbonImmutable::parse('2024-01-02'));
     expect($result)->toBeNull();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+
 });
