2d5ad03d44a8ce1747c061f0a857f591c2a090da
Felix Frey <felix.frey@gmx.at>
2024-01-17 14:55:39 +0100

    Implement holidays for Austria

---
+++ src/Countries/Austria.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Austria extends Country
+{
+    public function countryCode(): string
+    {
+        return 'at';
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Neujahr' => '01-01',
+            'Heilige Drei Könige' => '01-06',
+            'Staatsfeiertag' => '05-01',
+            'Mariä Himmelfahrt' => '08-15',
+            'Nationalfeiertag' => '10-26',
+            'Allerheiligen' => '11-01',
+            'Mariä Empfängnis' => '12-08',
+            'Christtag' => '12-25',
+            'Stefanitag' => '12-26',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('Europe/Vienna');
+
+        return [
+            'Ostermontag' => $easter->addDay(1),
+            'Christi Himmelfahrt' => $easter->addDays(39),
+            'Pfingstmontag' => $easter->addDays(50),
+            'Fronleichnam' => $easter->addDays(60),
+        ];
+    }
+}
