a7e886d41485f15f46bd25d57a4c5d70f297f77b
Marc Hanlon <marchanlon@gmail.com>
2024-01-17 22:33:53 +0000

    Add tests for St Brigid's Day
    St Brigids Day is a new holiday in 2023 and it is variable so adding tests around the logic

--- tests/Countries/IrelandTest.php
+++ tests/Countries/IrelandTest.php
@@ -17,3 +17,14 @@ it('can calculate irish holidays', function () {
     expect(formatDates($holidays))->toMatchSnapshot();
 
 });
+
+it('can calculate St Bridid\'s day in Ireland', function () {
+    // In 2022, there was no St Brigid's Day
+    expect(Holidays::for(country: 'ie')->isHoliday('2022-02-07'))->toBe(false);
+
+    // In 2023, it was on Monday 6th Feb
+    expect(Holidays::for(country: 'ie')->isHoliday('2023-02-06'))->toBe(true);
+
+    // In 2030, it will fall on Friday 1st Feb
+    expect(Holidays::for(country: 'ie')->isHoliday('2030-02-01'))->toBe(true);
+});
