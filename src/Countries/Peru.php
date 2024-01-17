1c15c628ac4b3a83c6c404ca8a57928943335347
Daniel López Sánchez <dlopezs2009@hotmail.com>
2024-01-17 18:48:46 -0500

    Adds Peru holidays

---
+++ src/Countries/Peru.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Peru extends Country
+{
+    public function countryCode(): string
+    {
+        return 'pe';
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Año nuevo' => '01-01',
+            'Día del Trabajo' => '05-01',
+            'Batalla de Arica y Día de la bandera' => '06-07',
+            'Día de San Pedro y San Pablo' => '06-29',
+            'Día de la Fuerza Aérea del Perú' => '07-23',
+            'Día de la Independencia' => '07-28',
+            'Fiestas Patrias' => '07-29',
+            'Batalla de Junín' => '08-06',
+            'Santa Rosa de Lima' => '08-30',
+            'Combate de Angamos' => '10-08',
+            'Día de Todos los Santos' => '11-01',
+            'Inmaculada Concepción' => '12-08',
+            'Batalla de Ayacucho' => '12-09',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('America/Lima');
+
+        return [
+            'Jueves Santo' => $easter->subDays(3),
+            'Viernes Santo' => $easter->subDays(2),
+        ];
+    }
+}
