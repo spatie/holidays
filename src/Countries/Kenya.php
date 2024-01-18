f808b196e548e070e6045cd40f5c765cb7acea7c
Nyagus <phil.nyaga@gmail.com>
2024-01-18 19:03:16 +0300

    fix:phpstan errors

--- src/Countries/Kenya.php
+++ src/Countries/Kenya.php
@@ -11,7 +11,7 @@ class Kenya extends Country
         return 'ke';
     }
 
-    /** @return array<string, CarbonImmutable> */
+    /** @return array<string, CarbonImmutable|string> */
     protected function allHolidays(int $year): array
     {
         return array_merge([
