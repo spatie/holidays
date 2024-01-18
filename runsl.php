66c5075e3cf5fc57349c9bef2cb161dfe10dea82
AvishkaSen <avishkasenanayake@hotmail.com>
2024-01-18 21:36:41 +0530

    working full moon day calculator

---
+++ runsl.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php'; // Adjust this path if necessary
+
+use Spatie\Holidays\Countries\SriLanka;
+
+$srilanka = new SriLanka();
+$srilanka->variableHolidays(2024);
--- src/Countries/SriLanka.php
+++ src/Countries/SriLanka.php
@@ -2,6 +2,9 @@
 
 namespace Spatie\Holidays\Countries;
 
+use DateTime;
+use Solaris\MoonPhase;
+
 class SriLanka extends Country
 {
     public function countryCode(): string
@@ -11,12 +14,57 @@ class SriLanka extends Country
 
     protected function allHolidays(int $year): array
     {
-        // TODO: Implement allHolidays() method.
+        return array_merge
+        ([
+            'Thai Pongal Day' => '01-15',
+            'Independence Day' => '02-04',
+            'Mahasivarathri Day' => '03-04',
+            'Good Friday' => '04-10',
+            'Day prior to Sinhala & Tamil New Year Day' => '04-13',
+            'Sinhala & Tamil New Year Day' => '04-14',
+            'May Day' => '05-01',
+            'Id-Ul-Fitr' => '05-24',
+            'Id-Ul-Alha' => '07-31',
+            'Milad-Un-Nabi' => '10-30',
+            'Deepavali Festival Day' => '11-14',
+            'Christmas' => '12-25',
+        ], $this->variableHolidays($year));
     }
 
-    protected function variableHolidays(int $year): array
+    function variableHolidays(int $year): array
     {
+        $this->getFullMoons($year);
 
+
+    }
+
+
+    function getFullMoons(int $year) : array
+    {
+        // $moonPhase = (new MoonPhase());
+
+        // gets the UNIX timestamp of the full moons
+        //$moon = $moonPhase->getPhaseNextFullMoon();
+
+        // $date = new DateTime('@' . $moon);
+
+        $start = new DateTime($year . '-01-01');
+        $end = new DateTime($year . '-12-31');
+
+        $fullMoons = [];
+
+        for ($date = $start; $date <= $end; $date->modify('+1 day')) {
+
+            $moonPhase = new MoonPhase($date);
+
+            $nextFullMoon = new DateTime('@' . $moonPhase->getPhaseNextFullMoon());
+
+            if(!in_array($nextFullMoon, $fullMoons)) {
+                $fullMoons[] = $nextFullMoon;
+            }
+        }
+
+        dd($fullMoons);
     }
 
 }
