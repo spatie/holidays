a2b7208e1f70f50176cb83dd5ea7ee5027080a45
Dennis <hello@1nul.dev>
2024-01-18 09:15:59 +0100

    Fixed errors regarding the order of day/month and took into account that Great Prayer Day is no longer a public holiday in 2024.

--- src/Countries/Denmark.php
+++ src/Countries/Denmark.php
@@ -16,9 +16,9 @@ class Denmark extends Country
     {
         return array_merge([
             'Nytår' => '01-01',
-            'Juleaften' => '24-12',
-            'Juledag' => '25-12',
-            'Anden Juledag' => '26-12'
+            'Juleaften' => '12-24',
+            'Juledag' => '12-25',
+            'Anden Juledag' => '12-26'
         ], $this->variableHolidays($year));
     }
 
@@ -28,7 +28,7 @@ class Denmark extends Country
         $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
             ->setTimezone('Europe/Copenhagen');
 
-        return [
+        $holidays = [
             'Påskedag' => $easter->addDay(),
             'Skærtorsdag' => $easter->subDays(3),
             'Langfredag' => $easter->subDays(2),
@@ -37,5 +37,11 @@ class Denmark extends Country
             'Pinse' => $easter->addDays(49),
             'Anden Pinsedag' => $easter->addDays(50),
         ];
+
+        if ($year < 2024) {
+            $holidays['Store Bededag'] = $easter->addDays(26);
+        }
+
+        return $holidays;
     }
 }
