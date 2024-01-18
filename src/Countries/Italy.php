a55ac54b07c5f0c4a46c2abe83444ad07d5d2db7
mauricius <maurizio.bonani@gmail.com>
2024-01-18 14:20:51 +0100

    remove: phpdoc

--- src/Countries/Italy.php
+++ src/Countries/Italy.php
@@ -11,7 +11,6 @@ class Italy extends Country
         return 'it';
     }
 
-    /** @return array<string, CarbonImmutable> */
     protected function allHolidays(int $year): array
     {
         return array_merge([
