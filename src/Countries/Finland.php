7d54b6a661dda4dea1916608f2fed2394365abf5
Conny Sjöblom <conny.sjoblom@lyyti.com>
2024-01-18 11:55:09 +0200

    Add holidays for Finland

---
+++ src/Countries/Finland.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Finland extends Country
+{
+    public function countryCode(): string
+    {
+        return 'fi';
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge($this->fixedHolidays($year), $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function fixedHolidays(int $year): array
+    {
+        return [
+            'Uudenvuodenpäivä' => CarbonImmutable::create($year, 1, 1),
+            'Loppiainen' => CarbonImmutable::create($year, 1, 6),
+            'Vappu' => CarbonImmutable::create($year, 5, 1),
+            'Itsenäisyyspäivä' => CarbonImmutable::create($year, 12, 6),
+            'Joulupäivä' => CarbonImmutable::create($year, 12, 25),
+            'Tapaninpäivä' => CarbonImmutable::create($year, 12, 26),
+        ];
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('Europe/Stockholm');
+
+        $midsummerDay = CarbonImmutable::create($year, 6, 20)->next(CarbonImmutable::SATURDAY);
+
+        return [
+            'Pitkäperjantai' => $easter->subDays(2),
+            'Pääsiäispäivä' => $easter,
+            'Toinen pääsiäispäivä' => $easter->addDay(),
+            'Helatorstai' => $easter->addDays(39),
+            'Helluntaipäivä' => $easter->addDays(49),
+            'Juhannuspäivä' => $midsummerDay->day > 26
+                ? $midsummerDay->subWeek()
+                : $midsummerDay,
+            'Pyhäinpäivä' => CarbonImmutable::create($year, 10, 31)
+                ->next(CarbonImmutable::SATURDAY),
+        ];
+    }
+}
