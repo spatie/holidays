d09db5afbdc2e506a56263a3d584cfc2a34fbb14
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-01-18 12:15:11 +0100

    remove annotation

--- src/Countries/Denmark.php
+++ src/Countries/Denmark.php
@@ -11,7 +11,6 @@ class Denmark extends Country
         return 'da';
     }
 
-    /** @return array<string, CarbonImmutable> */
     protected function allHolidays(int $year): array
     {
         return array_merge([
