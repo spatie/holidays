0b0a02f7231c0c4031c1483acbd4aee1f86e69da
proghasan <proghasan@yahoo.com>
2024-01-19 02:48:29 +0600

    Added Kuwait holidays

---
+++ src/Countries/Kuwait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Kuwait extends Country
+{
+    public function countryCode(): string
+    {
+        return 'kw';
+    }
+
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'New Year\'s Day' => '01-01',
+            'National Day' => '02-25',
+            'Liberation Day' => '02-26',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('Asia/Kuwait');
+
+        // If there are any specific variable holidays for Kuwait, you can add them here.
+        return [];
+    }
+}
