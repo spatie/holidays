d21390fc396de2be1bb425d07e9b659938425b80
knubbe <knubbe1982@gmail.com>
2024-01-18 22:55:05 +0100

    Add Serbian holidays

---
+++ src/Countries/Serbia.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Serbia extends Country
+{
+    protected function __construct(
+        public ?string $region = null
+    ) {
+    }
+
+    public function countryCode(): string
+    {
+        return 'sr';
+    }
+
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Nova godina - prvi dan' => '01-01',
+            'Nova godina - drugi dan' => '01-02',
+            'Dan državnosti - prvi dan' => '02-15',
+            'Dan državnosti - drugi dan' => '02-16',
+            'Praznik rada - prvi dan' => '05-01',
+            'Praznik rada - drugi dan' => '05-02',
+            'Dan primirja u Prvom svetskom ratu' => '11-11',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
+            ->setTimezone('Europe/Belgrade');
+
+        return [
+            'Vaskršnji ponedeljak' => $easter->addDay(),
+            'Veliki petak' => $easter->subDays(2),
+        ];
+    }
+}
