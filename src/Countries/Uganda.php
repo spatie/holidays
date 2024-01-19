a094266657d915ad94677f33c2d243d9b774d526
freekmurze <freekmurze@users.noreply.github.com>
2024-01-19 13:21:46 +0000

    Fix styling

--- src/Countries/Uganda.php
+++ src/Countries/Uganda.php
@@ -11,46 +11,44 @@ use Carbon\CarbonImmutable;
 
 class Uganda extends Country
 {
-
     public function countryCode(): string
     {
         return 'ug';
     }
 
     /**
-     * @inheritDoc
+     * {@inheritDoc}
      */
     protected function allHolidays(int $year): array
     {
 
-    /**
-    * 'New Year' => '01-01', Format:: Month-Date
-    */
+        /**
+         * 'New Year' => '01-01', Format:: Month-Date
+         */
         return array_merge([
-            "New Year's  Day" => "01-01",
-            "NRM Liberation Day" => "01-26",
-            "Archbishop Janani Luwum Day" => "02-16",
-            "International Women's Day" => "03-08",
-            "Labour Day" => "05-01",
-            "Martyrs' Day" => "06-03",
-            "National Hereos Day" => "06-09",
-            "Independence Day" => "10-09",
-            "Christmas Day" => "12-25",
-            "Boxing Day" => "12-26",
+            "New Year's  Day" => '01-01',
+            'NRM Liberation Day' => '01-26',
+            'Archbishop Janani Luwum Day' => '02-16',
+            "International Women's Day" => '03-08',
+            'Labour Day' => '05-01',
+            "Martyrs' Day" => '06-03',
+            'National Hereos Day' => '06-09',
+            'Independence Day' => '10-09',
+            'Christmas Day' => '12-25',
+            'Boxing Day' => '12-26',
         ], $this->variableHolidays($year));
     }
 
     /**
-     * @param int $year
-     *
      * @return array<string, CarbonImmutable>
      */
     private function variableHolidays(int $year): array
     {
         $easter = CarbonImmutable::createFromTimestamp(easter_date($year))->setTimezone('Africa/Nairobi');
+
         return [
-            "Good Friday" => $easter->subDays(2),
-            "Easter Monday" => $easter->addDay(),
+            'Good Friday' => $easter->subDays(2),
+            'Easter Monday' => $easter->addDay(),
         ];
     }
 }
