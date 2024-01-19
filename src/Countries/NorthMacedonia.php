27ab746e9df285ce88d0fa7f969ce1b367fd8f66
freekmurze <freekmurze@users.noreply.github.com>
2024-01-19 13:22:47 +0000

    Fix styling

--- src/Countries/NorthMacedonia.php
+++ src/Countries/NorthMacedonia.php
@@ -6,7 +6,6 @@ use Carbon\CarbonImmutable;
 
 class NorthMacedonia extends Country
 {
-
     public function countryCode(): string
     {
         return 'mk';
@@ -41,7 +40,7 @@ class NorthMacedonia extends Country
     protected function ortodoxEaster(int $year): bool|int
     {
         $timestamp = easter_date($year, CAL_EASTER_ALWAYS_JULIAN);
-        $daysDifference = (int)($year / 100) - (int)($year / 400) - 2;
+        $daysDifference = (int) ($year / 100) - (int) ($year / 400) - 2;
 
         return strtotime("+$daysDifference days", $timestamp);
     }
