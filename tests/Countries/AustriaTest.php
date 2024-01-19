af14fa2a5195467777bd92f5d5f49b5a0d16a59a
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-01-19 16:23:47 +0100

    use easter() instead

--- tests/Countries/AustriaTest.php
+++ tests/Countries/AustriaTest.php
@@ -17,15 +17,3 @@ it('can calculate austrian holidays', function () {
 
     expect(formatDates($holidays))->toMatchSnapshot();
 });
-
-it('can calculate austrian holidays with region holidays', function () {
-    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
-
-    $holidays = Holidays::for(Austria::make('bg'))->get();
-
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
-
-    expect(formatDates($holidays))->toMatchSnapshot();
-})->skip('Austria class has to be extended with regions first.');
