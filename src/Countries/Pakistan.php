de1d7ef89bbbbf782748cf7d036d5fcd302527b7
Hammad Zafar <hammadarain05@gmail.com>
2024-01-19 16:17:41 +0500

    Add Pakistan Holidays

---
+++ src/Countries/Pakistan.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Pakistan extends Country
+{
+    protected function __construct(
+        public ?string $region = null
+    ) {
+    }
+
+    public function countryCode(): string
+    {
+        return 'pk';
+    }
+
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Kashmir Solidarity Day'    => '02-05',
+            'Pakistan Resolution Day'   => '03-23',
+            'Labour Day'                => '05-01',
+            'Independence Day'          => '08-14',
+            'Iqbal Day'                 => '11-09',
+            'Quaid-e-Azam Day'          => '12-25',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        // As Islamic holidays are lunar based.
+        // https://github.com/spatie/holidays/discussions/79
+
+        return [];
+    }
+}
