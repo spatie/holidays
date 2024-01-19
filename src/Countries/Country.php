036096153365ab3792f4fc55d29d471707767cc3
Stephen S. Musoke <ssmusoke@gmail.com>
2024-01-19 18:16:08 +0300

    Add tests for computing the dates for Eid al-Fitr and Eid al-Adha

--- src/Countries/Country.php
+++ src/Countries/Country.php
@@ -3,6 +3,7 @@
 namespace Spatie\Holidays\Countries;
 
 use Carbon\CarbonImmutable;
+use GeniusTS\HijriDate\Date;
 use Spatie\Holidays\Exceptions\InvalidYear;
 use Spatie\Holidays\Exceptions\UnsupportedCountry;
 
@@ -90,5 +91,33 @@ abstract class Country
         if ($year > 2037) {
             throw InvalidYear::yearTooHigh();
         }
+    }
+
+    public static function eidAlFitr(int $year): CarbonImmutable {
+        // get the Hirji date for the first day of the Gregorian year
+        $hirji_date = \GeniusTS\HijriDate\Hijri::convertToHijri(CarbonImmutable::create($year));
+        // Eid al-Fitr is on the first day of the 10th month in the calendar
+        if ($hirji_date->month > 10) {
+            // the date has passed to move to the next year
+            $hirji_date->year($hirji_date->year + 1);
+        }
+        $eid_al_fitr = new Date(1, 10, $hirji_date->year);
+
+        return CarbonImmutable::createFromDate( \GeniusTS\HijriDate\Hijri::convertToGregorian($eid_al_fitr->day, $eid_al_fitr->month,
+            $eid_al_fitr->year));
+    }
+
+    public static function eidAlAdha(int $year): CarbonImmutable {
+        // get the Hirji date for the first day of the Gregorian year
+        $hirji_date = \GeniusTS\HijriDate\Hijri::convertToHijri(CarbonImmutable::create($year));
+        // Eid al-Fitr is on the first day of the 10th month in the calendar
+        if ($hirji_date->month > 10) {
+            // the date has passed to move to the next year
+            $hirji_date->year($hirji_date->year + 1);
+        }
+        $eid_al_adha = new Date(10, 12, $hirji_date->year);
+
+        return CarbonImmutable::createFromDate( \GeniusTS\HijriDate\Hijri::convertToGregorian($eid_al_adha->day, $eid_al_adha->month,
+            $eid_al_adha->year));
     }
 }
