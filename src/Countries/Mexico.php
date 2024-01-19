8185ca72591faaaf508e41f5986beb71d61f6c60
Dante Bazaldua <dante@spot2.mx>
2024-01-19 15:54:09 -0600

    feat(Mexico.php): rebase main to put mandatory days

--- src/Countries/Mexico.php
+++ src/Countries/Mexico.php
@@ -4,7 +4,6 @@ namespace Spatie\Holidays\Countries;
 
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
-use Carbon\CarbonPeriod;
 
 class Mexico extends Country
 {
@@ -16,16 +15,13 @@ class Mexico extends Country
     protected function allHolidays(int $year): array
     {
         return array_merge([
-            'Año nuevo' => '01-01',
-            'Día de la Candelaria' => '02-02',
-            'Día de la Bandera' => '02-24',
-            'Día del Niño' => '04-30',
-            'Día de la Madre' => '04-30',
-            'Día del Trabajo' => '05-01',
-            'Día de la Independencia' => '09-15',
-            'Día de la Raza' => '10-12',
-            'Día de Muertos' => '11-02',
-            'Virgen de Guadalupe' => '12-12',
+            'Año Nuevo' => '01-01',
+            'Natalicio de Benito Juárez' => '04-18',
+            'Día Internacional de los Trabajadores' => '05-01',
+            'Jornada Electoral General' => '06-02',
+            'Día de Independencia' => '09-16',
+            'Cambio de Gobierno' => '10-01',
+            'Día de la Revolución' => '11-18',
             'Navidad' => '12-25',
         ], $this->variableHolidays($year));
     }
@@ -33,53 +29,13 @@ class Mexico extends Country
     /** @return array<string, CarbonImmutable> */
     protected function variableHolidays(int $year): array
     {
+        /** @phpstan-ignore-next-line */
+        $constitutionDay = Carbon::createFromFormat("d/m/Y", "01/02/" . $year)
+            ->firstOfMonth(1)
+            ->setTimezone('America/Mexico_City');
 
-        $natalicioBenitoJuarez = new CarbonImmutable(sprintf('third monday of march %s', $year));
-        $promulgacionConstitucion = new CarbonImmutable(sprintf('first monday of february %s', $year));
-        $revolucionMexicana = CarbonImmutable::createFromFormat('Y-m-d', "{$year}-11-20")->setTimezone('America/Mexico_City');
-
-        if ($revolucionMexicana->isSunday()) {
-            $revolucionMexicana = $revolucionMexicana->next('monday');
-        }
-
-        $fathersDay = new CarbonImmutable(sprintf('third sunday of june %s', $year));
-
-        $days = [
-            'Aniversario de la promulgación de la Constitución de 1917' => $promulgacionConstitucion->format('m-d'),
-            'Natalicio de Benito Juárez' => $natalicioBenitoJuarez->format('m-d'),
-            'Revolución Mexicana' => $revolucionMexicana->format('m-d'),
-            'Día del Padre' => $fathersDay->format('m-d'),
-
+        return [
+            'Día de la Constitución' => CarbonImmutable::createFromMutable($constitutionDay) // It's the first monday of february
         ];
-
-        if ($this->transmisionPoderEjecutivoFederal($year)) {
-            $days[
-                'Transmisión del Poder Ejecutivo Federal'
-            ] = $this->transmisionPoderEjecutivoFederal($year);
-        }
-
-        return $days;
-    }
-
-    protected function transmisionPoderEjecutivoFederal($year): bool|string
-    {
-        $period = new CarbonPeriod();
-        $period->setDateClass(CarbonImmutable::class);
-        $period
-            ->every('6 years')
-            ->since(sprintf('%s-10-01', 2024))
-            ->until(sprintf('%s-10-01 00:00:00', Carbon::now()->addYears(6)->year));
-
-        $period->addFilter(function ($date) use ($year) {
-            return $date->year === $year;
-        });
-
-        $availableDates = $period->toArray();
-
-        if (count($availableDates)) {
-            return $availableDates[0]->format('m-d');
-        }
-
-        return false;
     }
 }
