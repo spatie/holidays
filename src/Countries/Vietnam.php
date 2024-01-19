5997d03a9a022ad76a56ed3d4bae515e0b0aaaaa
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2024-01-19 12:49:38 +1100

    include Vietnamese holidays according to Chinese Calendar.

--- src/Countries/Vietnam.php
+++ src/Countries/Vietnam.php
@@ -3,9 +3,13 @@
 namespace Spatie\Holidays\Countries;
 
 use Carbon\CarbonImmutable;
+use DateTimeZone;
+use IntlDateFormatter;
 
 class Vietnam extends Country
 {
+    protected $timezone = 'Asia/Ho_Chi_Minh';
+
     public function countryCode(): string
     {
         return 'vi';
@@ -24,6 +28,29 @@ class Vietnam extends Country
     /** @return array<string, CarbonImmutable> */
     protected function variableHolidays(int $year): array
     {
-        return [];
+        return [
+            'Lunar New Year\'s Eve' => $this->chineseCalendar('01/01', $year),
+            'Lunar New Year Day 1' => $this->chineseCalendar('01/01', $year),
+            'Lunar New Year Day 2' => $this->chineseCalendar('01/02', $year),
+            'Lunar New Year Day 3' => $this->chineseCalendar('01/03', $year),
+            'Lunar New Year Day 4' => $this->chineseCalendar('01/04', $year),
+            'Hung Kings\' Festival' => $this->chineseCalendar('03/10', $year),
+        ];
+    }
+
+    protected function chineseCalendar(string $input, int $year): string
+    {
+        $formatter = new IntlDateFormatter(
+            locale: 'zh-CN@calendar=chinese',
+            dateType: IntlDateFormatter::SHORT,
+            timeType: IntlDateFormatter::NONE,
+            timezone: $this->timezone,
+            calendar: IntlDateFormatter::TRADITIONAL
+        );
+
+        $timeStamp = $formatter->parse($year . '/' . $input);
+        $dateTime = date_create()->setTimeStamp($timeStamp)->setTimezone(new DateTimeZone($this->timezone));
+
+        return $dateTime->format('m-d');
     }
 }
