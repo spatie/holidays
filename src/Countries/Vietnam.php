6ad7eb8dc730da1b21032291ff1e54ecdf5804cd
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2024-01-19 16:23:54 +1100

    use Vietnamese to name the holidays, English names commented.

--- src/Countries/Vietnam.php
+++ src/Countries/Vietnam.php
@@ -19,10 +19,14 @@ class Vietnam extends Country
     protected function allHolidays(int $year): array
     {
         return array_merge([
-            'New Year\'s day' => '01-01',
-            'Reunification Day' => '04-30',
-            'Labour Day' => '05-01',
-            'Independence Day' => '09-02',
+            // New Year's Day
+            'Tết Dương Lịch' => '01-01',
+            // Reunification Day
+            'Ngày Giải Phóng Miền Nam, Thống Nhất Đất Nước' => '04-30',
+            // Labour Day
+            'Ngày Quốc Tế Lao Động' => '05-01',
+            // Independence Day
+            'Ngày Quốc Khánh' => '09-02',
         ], $this->variableHolidays($year));
     }
 
@@ -30,12 +34,18 @@ class Vietnam extends Country
     protected function variableHolidays(int $year): array
     {
         return [
-            'Lunar New Year\'s Eve' => $this->chineseToGregorianDate('12/30', $year - 1),
-            'Lunar New Year Day 1' => $this->chineseToGregorianDate('01/01', $year),
-            'Lunar New Year Day 2' => $this->chineseToGregorianDate('01/02', $year),
-            'Lunar New Year Day 3' => $this->chineseToGregorianDate('01/03', $year),
-            'Lunar New Year Day 4' => $this->chineseToGregorianDate('01/04', $year),
-            'Hung Kings\' Festival' => $this->chineseToGregorianDate('03/10', $year),
+            // Lunar New Year's Eve
+            'Ngày Ba Mươi Tết' => $this->chineseToGregorianDate('12/30', $year - 1),
+            // Lunar New Year Day 1
+            'Mùng Một Tết Âm Lịch' => $this->chineseToGregorianDate('01/01', $year),
+            // Lunar New Year Day 2
+            'Mùng Hai Tết Âm Lịch' => $this->chineseToGregorianDate('01/02', $year),
+            // Lunar New Year Day 3
+            'Mùng Ba Tết Âm Lịch' => $this->chineseToGregorianDate('01/03', $year),
+            // Lunar New Year Day 4
+            'Mùng Bốn Tết Âm Lịch' => $this->chineseToGregorianDate('01/04', $year),
+            // Hung Kings' Festival
+            'Ngày Giỗ Tổ Hùng Vương' => $this->chineseToGregorianDate('03/10', $year),
         ];
     }
 }
