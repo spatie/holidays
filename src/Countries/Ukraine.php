6922f240f0ef4f21edb8e2c6876cfe419c8fedbe
Peter Khomyn <xkapi14@gmail.com>
2024-01-20 13:47:25 +0200

    Update Ukraine.php

--- src/Countries/Ukraine.php
+++ src/Countries/Ukraine.php
@@ -6,11 +6,6 @@ use Carbon\CarbonImmutable;
 
 class Ukraine extends Country
 {
-    protected function __construct(
-        public ?string $region = null
-    ) {
-    }
-
     public function countryCode(): string
     {
         return 'ua';
@@ -34,30 +29,11 @@ class Ukraine extends Country
     /** @return array<string, CarbonImmutable> */
     protected function variableHolidays(int $year): array
     {
-        $easter = CarbonImmutable::createFromTimestamp($this->orthodox_eastern($year))
-            ->setTimezone('Europe/Kyiv');
+        $easter = $this->orthodoxEaster($year);
 
         return [
             'Великодній Понеділок' => $easter->addDay(),
             'Трійця' => $easter->addDays(50),
         ];
-    }
-
-
-    protected function orthodox_eastern($year): string
-    {
-        /* Copy from https://php5.kiev.ua/php7/function.easter-date.html */
-
-        $a = $year % 4;
-        $b = $year % 7;
-        $c = $year % 19;
-        $d = (19 * $c + 15) % 30;
-        $e = (2 * $a + 4 * $b - $d + 34) % 7;
-        $month = floor(($d + $e + 114) / 31);
-        $day = (($d + $e + 114) % 31) + 1;
-
-        $de = mktime(0, 0, 0, $month, $day + 13, $year);
-
-        return $de;
     }
 }
