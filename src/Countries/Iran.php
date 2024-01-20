12c485bb530380bae355b38a4c4c8faab6be9802
aryaa <56627259+aryala7@users.noreply.github.com>
2024-01-20 12:59:29 +0000

    [fix] fix issues

--- src/Countries/Iran.php
+++ src/Countries/Iran.php
@@ -11,20 +11,19 @@ class Iran extends Country
         return 'ir';
     }
 
-    /** @return array<string, CarbonImmutable> */
     protected function allHolidays(int $year): array
     {
         return array_merge([
             'Islamic Revolution Day' => '02-11',
-            'Nourouz' => '03-19',
-            'National Oil Industry Day' => '03-20',
-            'Nourouz' => '03-21',
-            'Nourouz' => '03-22',
-            'Nourouz' => '03-23',
+            'Nationalization of Oil Industry Day' => '03-19',
+            'Nowruz First Day' => '03-20',
+            'Nowruz Second Day' => '03-21',
+            'Nowruz Third Day' => '03-22',
+            'Nowruz Fourth Day' => '03-23',
             'Islamic Republic Day' => '03-31',
             'Nature Day' => '04-01',
-            'Death of Khomeini' => '04-06',
-            '15th Khordad March' => '04-07',
+            'Death of Khomeini Day' => '06-03',
+            'Revolt of Khordad 15 Day' => '06-04',
         ], $this->variableHolidays($year));
     }
 
@@ -35,7 +34,7 @@ class Iran extends Country
             ->setTimezone('Asia/Tehran');
 
         return [
-          //add more holidays
+            //add more holidays
         ];
     }
 }
