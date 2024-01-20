583bd0f62e6850ed6f2286b72a13d9fd5d5cf29b
Robert Österlind <robert@informationsgruppen.se>
2024-01-20 15:23:32 +0100

    Update Sweden class

--- src/Countries/Sweden.php
+++ src/Countries/Sweden.php
@@ -11,23 +11,16 @@ class Sweden extends Country
         return 'se';
     }
 
-    /** @return array<string, CarbonImmutable> */
     protected function allHolidays(int $year): array
     {
-        return array_merge($this->fixedHolidays($year), $this->variableHolidays($year));
-    }
-
-    /** @return array<string, CarbonImmutable> */
-    protected function fixedHolidays(int $year): array
-    {
-        return [
-            'Nyårsdagen' => CarbonImmutable::create($year, 1, 1),
+        return array_merge([
+            'Nyårsdagen'       => CarbonImmutable::create($year, 1, 1),
             'Trettondedag jul' => CarbonImmutable::create($year, 1, 6),
-            'Första maj' => CarbonImmutable::create($year, 5, 1),
-            'Nationaldagen' => CarbonImmutable::create($year, 6, 6),
-            'Juldagen' => CarbonImmutable::create($year, 12, 25),
-            'Annandag jul' => CarbonImmutable::create($year, 12, 26),
-        ];
+            'Första maj'       => CarbonImmutable::create($year, 5, 1),
+            'Nationaldagen'    => CarbonImmutable::create($year, 6, 6),
+            'Juldagen'         => CarbonImmutable::create($year, 12, 25),
+            'Annandag jul'     => CarbonImmutable::create($year, 12, 26),
+        ], $this->variableHolidays($year));
     }
 
     /** @return array<string, CarbonImmutable> */
@@ -36,19 +29,28 @@ class Sweden extends Country
         $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
             ->setTimezone('Europe/Stockholm');
 
-        $midsummerDay = CarbonImmutable::create($year, 6, 20)->next(CarbonImmutable::SATURDAY);
+        $midsummerDay = CarbonImmutable::create($year, 6, 20);
+
+        if (!$midsummerDay->isSaturday()) {
+            $midsummerDay = $midsummerDay->next(CarbonImmutable::SATURDAY);
+        }
+
+        $halloween = CarbonImmutable::create($year, 10, 31);
+
+        if (!$halloween->isSaturday()) {
+            $halloween = $halloween->next(CarbonImmutable::SATURDAY);
+        }
 
         return [
-            'Långfredagen' => $easter->subDays(2),
-            'Påskdagen' => $easter,
-            'Annandag påsk' => $easter->addDay(),
+            'Långfredagen'           => $easter->subDays(2),
+            'Påskdagen'              => $easter,
+            'Annandag påsk'          => $easter->addDay(),
             'Kristi himmelsfärdsdag' => $easter->addDays(39),
-            'Pingstdagen' => $easter->addDays(49),
-            'Midsommardagen' => $midsummerDay->day > 26
+            'Pingstdagen'            => $easter->addDays(49),
+            'Midsommardagen'         => $midsummerDay->day > 26
                 ? $midsummerDay->subWeek()
                 : $midsummerDay,
-            'Alla helgons dag' => CarbonImmutable::create($year, 10, 31)
-                ->next(CarbonImmutable::SATURDAY),
+            'Alla helgons dag'       => $halloween,
         ];
     }
 }
