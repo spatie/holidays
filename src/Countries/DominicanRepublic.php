d30d0562eaf2618f2e659695f6a1269cefcdefec
Javier Emmanuel Mercedes <pikachu@Javiers-MacBook-Pro.local>
2024-01-19 20:06:27 -0400

    Fixed non national holiday

--- src/Countries/DominicanRepublic.php
+++ src/Countries/DominicanRepublic.php
@@ -14,7 +14,7 @@ class DominicanRepublic extends Country
     /** @return array<string, CarbonImmutable | string> */
     protected function allHolidays(int $year): array
     {
-        return array_merge([
+        return [
             'Año Nuevo' => '01-01',
             'Día de la Altagracia' => '01-21',
             'Día de Duarte' => '01-26',
@@ -23,20 +23,7 @@ class DominicanRepublic extends Country
             'Día de la Restauración' => '08-16',
             'Día de las Mercedes' => '09-24',
             'Día de la Constitución' => '11-06',
-            'Día de la Virgen de la Altagracia' => '12-08',
             'Navidad' => '12-25',
-        ], $this->variableHolidays($year));
-    }
-
-    /** @return array<string, CarbonImmutable> */
-    protected function variableHolidays(int $year): array
-    {
-        $easter =  CarbonImmutable::createFromTimestamp(easter_date($year))
-            ->setTimezone('America/Santo_Domingo');
-
-        return [
-            'Jueves Santo' => $easter->subDays(3),
-            'Viernes Santo' => $easter->subDays(2),
         ];
     }
 }
