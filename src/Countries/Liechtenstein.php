6d409e95c622d9c4fcd0684e94ed0dbd3722323d
Martin Welte <martin.welte@towa.at>
2024-01-21 16:45:02 +0100

    feat: add liechtenstein

---
+++ src/Countries/Liechtenstein.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Liechtenstein extends Country
+{
+    public function countryCode(): string
+    {
+        return 'li';
+    }
+
+    /** @return array<string, string|CarbonImmutable> */
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Neujahr' => '01-01',
+            'Heilige Drei Könige' => '01-06',
+            'Tag der Arbeit' => '05-01',
+            'Staatsfeiertag / Mariä Himmelfahrt' => '08-15',
+            'Mariä Geburt' => '09-08',
+            'Allerheiligen' => '11-01',
+            'Mariä Empfängnis' => '12-08',
+            'Weihnachten' => '12-25',
+            'Stephanstag' => '12-26',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = CarbonImmutable::createFromTimestamp($this->easter($year))
+            ->setTimezone('Europe/Vaduz');
+
+        return [
+            'Ostermontag' => $easter->addDay(),
+            'Auffahrt' => $easter->addDays(39),
+            'Pfingstmontag' => $easter->addDays(50),
+            'Fronleichnam' => $easter->addDays(60),
+        ];
+    }
+}
