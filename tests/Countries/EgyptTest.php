1e867de0dcab7e56d92476b86e1af4b42f2ecd68
Wessam Ahmed <wessam@cobbleweb.co.uk>
2024-01-21 16:25:23 +0200

    For consistency

--- tests/Countries/EgyptTest.php
+++ tests/Countries/EgyptTest.php
@@ -5,7 +5,7 @@ namespace Spatie\Holidays\Tests\Countries;
 use Carbon\CarbonImmutable;
 use Spatie\Holidays\Holidays;
 
-it('can calculate egypt holidays 2024', function () {
+it('can calculate egypt holidays', function () {
     CarbonImmutable::setTestNowAndTimezone('2024-01-01', 'Africa/Cairo');
 
     $holidays = Holidays::for(country: 'eg')->get();
