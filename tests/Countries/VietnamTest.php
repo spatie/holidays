96f6c8288195f52fd902e93298e50c4c7369ef82
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2024-01-22 18:52:08 +1100

    add a few test cases to make sure the Lunar New Year holiday in Vietnam is calculated correctly.

--- tests/Countries/VietnamTest.php
+++ tests/Countries/VietnamTest.php
@@ -17,3 +17,21 @@ it('can calculate vietnamese holidays', function () {
 
     expect(formatDates($holidays))->toMatchSnapshot();
 });
+
+it('can calculate the Lunar New Year holiday', function ($year, $expectedHoliday) {
+    CarbonImmutable::setTestNowAndTimezone("{$year}-01-01", 'Asia/Ho_Chi_Minh');
+
+    $holidays = Holidays::for(country: 'vn')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    $lunarNewYearHoliday = array_map(fn ($date) => $date['date'], formatDates($holidays));
+
+    expect($lunarNewYearHoliday)->toContain(...$expectedHoliday);
+})->with([
+    ['year' => 2023, 'expected_holiday' => ['2023-01-21', '2023-01-22', '2023-01-23', '2023-01-24', '2023-01-25']],
+    ['year' => 2021, 'expected_holiday' => ['2021-02-11', '2021-02-12', '2021-02-13', '2021-02-14', '2021-02-15']],
+    ['year' => 2019, 'expected_holiday' => ['2019-02-04', '2019-02-05', '2019-02-06', '2019-02-07', '2019-02-08']],
+]);
