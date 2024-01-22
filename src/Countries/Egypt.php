d66b66d1c231886ced77e2e15b820bb4727163e6
Wessam Ahmed <wessam@cobbleweb.co.uk>
2024-01-22 11:41:31 +0200

    Early return

--- src/Countries/Egypt.php
+++ src/Countries/Egypt.php
@@ -135,19 +135,22 @@ class Egypt extends Country
     {
         $adjustedHolidays = [];
 
-        // Explicitly define this logic to avoid timezone confusion on the CarbonInterface::next() method
-        if ($date) {
-            if ($date->isFriday() || $date->isSaturday()) {
-                // If the holiday falls on a weekend (Friday or Saturday), it is observed on the following Sunday
-                $adjustedHolidays['Day off for '.$name] = $date->next(CarbonInterface::SUNDAY);
-            } elseif ($date->isSunday() || $date->isThursday()) {
-                // If the holiday falls on a Sunday or Thursday, it is observed on the same day
-                $adjustedHolidays[$name] = $date;
-            } else {
-                // If the holiday falls on a weekday (Monday, Tuesday, Wednesday), it is observed on the following Thursday
-                $adjustedHolidays['Day off for '.$name] = $date->next(CarbonInterface::THURSDAY);
-            }
+        if (! $date instanceof CarbonImmutable) {
+            return [];
         }
+
+        // Explicitly define this logic to avoid timezone confusion on the CarbonInterface::next() method
+        if ($date->isFriday() || $date->isSaturday()) {
+            // If the holiday falls on a weekend (Friday or Saturday), it is observed on the following Sunday
+            $adjustedHolidays['Day off for '.$name] = $date->next(CarbonInterface::SUNDAY);
+        } elseif ($date->isSunday() || $date->isThursday()) {
+            // If the holiday falls on a Sunday or Thursday, it is observed on the same day
+            $adjustedHolidays[$name] = $date;
+        } else {
+            // If the holiday falls on a weekday (Monday, Tuesday, Wednesday), it is observed on the following Thursday
+            $adjustedHolidays['Day off for '.$name] = $date->next(CarbonInterface::THURSDAY);
+        }
+
 
         return $adjustedHolidays;
     }
