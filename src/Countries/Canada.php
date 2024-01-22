72373b7558720dd572e91a8677618951a9e35a79
Marc Cocchio <marccocchio@gmail.com>
2024-01-22 16:05:15 +0900

    Add Canada country support

---
+++ src/Countries/Canada.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Canada extends Country
+{
+    public function countryCode(): string
+    {
+        return 'ca';
+    }
+
+    // These 5 statutory holidays are the only constant among all provinces
+    // All other holidays vary from province to province.
+
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'New Year\'s Day' => '01-01',
+            'Canada Day' => '07-05',
+            'Christmas Day' => '12-25',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, string|CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $labourDay = new CarbonImmutable("first monday of september {$year}");
+
+        $easter = $this->easter($year);
+
+        return [
+            'Good Friday' => $easter->subDays(2),
+            'Labour Day' => $labourDay,
+        ];
+    }
+}
