71169b8b1fc4832ddcf30030b3a27d7c57a14a74
freekmurze <freekmurze@users.noreply.github.com>
2024-01-23 14:28:59 +0000

    Fix styling

--- src/Countries/Greece.php
+++ src/Countries/Greece.php
@@ -14,13 +14,13 @@ class Greece extends Country
     protected function allHolidays(int $year): array
     {
         return array_merge([
-            'Πρωτοχρονιά'         => '01-01',
-            'Θεοφάνια'            => '01-06',
-            '25η Μαρτίου'         => '03-25',
-            'Πρωτομαγιά'          => '05-01',
-            'Δεκαπενταύγουστος'   => '08-15',
-            '28η Οκτωβρίου'       => '10-28',
-            'Χριστούγεννα'        => '12-25',
+            'Πρωτοχρονιά' => '01-01',
+            'Θεοφάνια' => '01-06',
+            '25η Μαρτίου' => '03-25',
+            'Πρωτομαγιά' => '05-01',
+            'Δεκαπενταύγουστος' => '08-15',
+            '28η Οκτωβρίου' => '10-28',
+            'Χριστούγεννα' => '12-25',
             'Σύναξη της Θεοτόκου' => '12-26',
 
         ], $this->variableHolidays($year));
@@ -30,7 +30,7 @@ class Greece extends Country
     protected function variableHolidays(int $year): array
     {
         // OrthodoxEaster needs to setTimezone
-        $orthodoxEaster = $this->orthodoxEaster($year)->setTimezone("Europe/Athens");
+        $orthodoxEaster = $this->orthodoxEaster($year)->setTimezone('Europe/Athens');
         $cleanMonday = $orthodoxEaster->copy()->subDays(48);
         $megaliParaskevi = $orthodoxEaster->copy()->subDays(2);
         $megaloSavvato = $orthodoxEaster->copy()->subDays(1);
@@ -41,7 +41,7 @@ class Greece extends Country
         $protomagia = CarbonImmutable::createFromFormat('Y-m-d', "{$year}-05-01");
         $moveProtomagia = [$megaliParaskevi, $megaloSavvato, $orthodoxEaster, $deuteraPasha];
 
-        if ( in_array($protomagia, $moveProtomagia) ) {
+        if (in_array($protomagia, $moveProtomagia)) {
             $protomagia = $orthodoxEaster->copy()->addDays(2);
         }
         if ($protomagia->isSunday()) {
@@ -49,12 +49,12 @@ class Greece extends Country
         }
 
         return [
-            'Καθαρά Δευτέρα'    => $cleanMonday, //always Monday
-            'Πρωτομαγιά'        => $protomagia,
-            'Μεγάλη Παρασκευή'  => $megaliParaskevi,
+            'Καθαρά Δευτέρα' => $cleanMonday, //always Monday
+            'Πρωτομαγιά' => $protomagia,
+            'Μεγάλη Παρασκευή' => $megaliParaskevi,
             'Κυριακή του Πάσχα' => $orthodoxEaster,
             'Δευτέρα του Πάσχα' => $deuteraPasha,
-            'Αγίου Πνεύματος'   => $agiouPneumatos, //always Monday
+            'Αγίου Πνεύματος' => $agiouPneumatos, //always Monday
         ];
     }
 }
