8c9756c0fc0c12ccb5543690ef40fd2c7b6d8e14
Ezequiel Dias <ezequidias@outlook.com>
2024-01-24 02:05:59 -0300

    Adding regional state holidays

--- src/Countries/Brazil.php
+++ src/Countries/Brazil.php
@@ -6,6 +6,11 @@ use Carbon\CarbonImmutable;
 
 class Brazil extends Country
 {
+    protected function __construct(
+        protected ?string $region = null,
+    ) {
+    }
+
     public function countryCode(): string
     {
         return 'br';
@@ -23,7 +28,7 @@ class Brazil extends Country
             'Proclamação da República' => '11-15',
             'Dia Nacional de Zumbi e da Consciência Negra' => '11-20',
             'Natal' => '12-25',
-        ], $this->variableHolidays($year));
+        ], $this->variableHolidays($year), $this->regionHolidays($year));
     }
 
     /** @return array<string, CarbonImmutable> */
@@ -36,5 +41,29 @@ class Brazil extends Country
             'Sexta-feira Santa' => $easter->subDays(2),
             'Corpus Christi' => $easter->addDays(60),
         ];
+    }
+
+    /** @return array<string, CarbonImmutable|string> */
+    protected function regionHolidays(int $year): array
+    {
+        switch ($this->region) {
+            case 'BR-SP':
+                return [
+                    'Revolução Constitucionalista' => '05-09',
+                ];
+            case 'BR-SP-3550308':
+                return [
+                    'Aniversário da cidade de São Paulo' => '01-25',
+                    'Corpus Christi' => '30-4',
+                ];
+            case 'BR-RJ':
+                return [
+                    'Carnaval' => '02-13',
+                    'Dia de São Jorge' => '04-23',
+                ];
+        }
+
+
+        return [];
     }
 }
