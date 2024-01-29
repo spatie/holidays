f425d4945d0f1cf69a3ea7186bed653c007ecb96
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-01-29 20:00:16 +0100

    Add failing tests: - Missing labor day - Special condition holidays

--- tests/Countries/SwitzerlandTest.php
+++ tests/Countries/SwitzerlandTest.php
@@ -7,6 +7,41 @@ use Spatie\Holidays\Countries\Switzerland;
 use Spatie\Holidays\Exceptions\InvalidRegion;
 use Spatie\Holidays\Holidays;
 
+dataset('cantons', [
+    'ag',
+    'ai',
+    'ar',
+    'be',
+    'bl',
+    'bs',
+    'fr',
+    'ge',
+    'gl',
+    'gr',
+    'ju',
+    'lu',
+    'ne',
+    'nw',
+    'ow',
+    'sg',
+    'sh',
+    'so',
+    'sz',
+    'tg',
+    'ti',
+    'ur',
+    'vd',
+    'vs',
+    'zg',
+    'zh',
+]);
+
+dataset('languages', [
+    'de',
+    'fr',
+    'it'
+]);
+
 it('can calculate swiss holidays', function () {
     CarbonImmutable::setTestNowAndTimezone('2024-01-01');
 
@@ -19,10 +54,10 @@ it('can calculate swiss holidays', function () {
     expect(formatDates($holidays))->toMatchSnapshot();
 });
 
-it('can get swiss holidays for a specified region (zh)', function () {
+it('can calculate holidays for a specified canton', function ($canton) {
     CarbonImmutable::setTestNowAndTimezone('2024-01-01');
 
-    $switzerland = new Switzerland(region: 'ch-zh');
+    $switzerland = new Switzerland(region: "ch-$canton");
 
     $holidays = Holidays::for($switzerland)->get();
 
@@ -31,28 +66,79 @@ it('can get swiss holidays for a specified region (zh)', function () {
         ->not()->toBeEmpty();
 
     expect(formatDates($holidays))->toMatchSnapshot();
-});
+})->with('cantons');
+
 
 it('throws an error when an invalid region is given', function () {
-    new Switzerland('ch-xx');
+    $switzerland = new Switzerland('ch-xx');
+    $holidays = Holidays::for($switzerland)->get();
+
 })->throws(InvalidRegion::class);
 
-it('can translate swiss holidays into french', function () {
-    $holidays = Holidays::for(country: 'ch', locale: 'fr', year: 2024)->get();
 
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
+it('does not have ephinay holiday at zh', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
 
-    expect(formatDates($holidays))->toMatchSnapshot();
+    $switzerland = new Switzerland('ch-zh');
+    $holidays = Holidays::for($switzerland);
+
+    expect($holidays->isHoliday('2024-01-06'))->toBeFalse();
 });
 
-it('can translate swiss holidays into italian', function () {
-    $holidays = Holidays::for(country: 'ch', locale: 'it', year: 2024)->get();
+describe('special holidays with conditions', function () {
 
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
+    it('has holidays in on 12-26 if christmas is not on a monday or friday at', function (string $canton, int $year, bool $isHoliday) {
+        CarbonImmutable::setTestNowAndTimezone("$year-01-01");
 
-    expect(formatDates($holidays))->toMatchSnapshot();
+        $switzerland = new Switzerland("ch-$canton");
+        $holidays = Holidays::for($switzerland);
+
+        expect($holidays->isHoliday("$year-12-26"))->toBe($isHoliday);
+    })
+        ->with(['ai', 'ar'])
+        ->with([
+            [2017, false],
+            [2023, false],
+            [2024, true],
+            [2025, true],
+            [2026, false],
+        ]);
+
+    it('has sets jeune genevois to thursday following the first sunday in september', function (int $year, string $date) {
+        CarbonImmutable::setTestNowAndTimezone("$year-01-01");
+
+        $switzerland = new Switzerland("ch-ge");
+        $holidays = Holidays::for($switzerland);
+
+        expect($holidays->isHoliday($date))->toBeTrue();
+    })
+        ->with([
+            [2017, '2017-09-07'],
+            [2023, '2023-09-07'],
+            [2024, '2024-09-05'],
+            [2025, '2025-09-11'],
+            [2026, '2026-09-10'],
+        ]);
 });
+
+it('celebrates labour day on 1st of may', function (string $canton) {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $switzerland = new Switzerland("ch-$canton");
+    $holidays = Holidays::for($switzerland);
+
+    expect($holidays->isHoliday('2024-05-01'))->toBeTrue();
+})
+    ->with([
+        'zh',
+        'so',
+        'bs',
+        'bl',
+        'sh',
+        'ag',
+        'tg',
+        'ne',
+        'ju',
+        'ti'
+    ]);
+
