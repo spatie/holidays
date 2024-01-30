4c9538e00134f1873eeb7b88f1a021ec2b2648b8
shoxrux1996 <shohrux.shomaxmudov@gmail.com>
2024-01-30 19:42:31 +0500

    feat: add locale & update islamic holidays

--- tests/Countries/UzbekistanTest.php
+++ tests/Countries/UzbekistanTest.php
@@ -5,10 +5,34 @@ namespace Spatie\Holidays\Tests\Countries;
 use Carbon\CarbonImmutable;
 use Spatie\Holidays\Holidays;
 
-it('can calculate uzbekistan holidays', function () {
-    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+it('can calculate uzbekistan holidays', function ($year) {
+    CarbonImmutable::setTestNow($year.'-01-01');
 
     $holidays = Holidays::for(country: 'uz')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+})->with([2000, 2006, 2023, 2024, 2033]);
+
+it('can calculate uzbekistan holidays in English', function () {
+    CarbonImmutable::setTestNow('2024-01-01');
+
+    $holidays = Holidays::for(country: 'uz', locale: 'en')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('can calculate uzbekistan holidays in Russian', function () {
+    CarbonImmutable::setTestNow('2024-01-01');
+
+    $holidays = Holidays::for(country: 'uz', locale: 'ru')->get();
 
     expect($holidays)
         ->toBeArray()
