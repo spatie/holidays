60df0844a3dd83923e2d6b483d45012180ecf7c2
Peter Sowah <petersowah@gmail.com>
2024-01-31 19:45:26 +0000

    Add public holidays for Ghana (#175)
    * add Ghana holidays
    
    * add iterable type to variableHolidays method
    
    * Update src/Countries/Ghana.php
    
    Co-authored-by: Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
    
    * clean
    
    ---------
    
    Co-authored-by: Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>

---
+++ tests/Countries/GhanaTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Spatie\Holidays\Tests\Countries;
+
+use Carbon\CarbonImmutable;
+use Spatie\Holidays\Countries\France;
+use Spatie\Holidays\Holidays;
+
+it('can calculate Ghana holidays', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'gh')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('can calculate Ghana easter based region holidays', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'gh')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('can calculate Ghana date based regional holidays', function () {
+    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+
+    $holidays = Holidays::for(country: 'gh')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
