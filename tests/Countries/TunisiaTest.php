a27dfe5b64a23e2faaa845432f2376bc5b326e48
GAYTH WS <gaith_baccari@hotmail.fr>
2024-01-31 11:03:33 +0100

    change Hijri Dates to generated Like in turkey holidays

--- tests/Countries/TunisiaTest.php
+++ tests/Countries/TunisiaTest.php
@@ -28,3 +28,14 @@ it('can calculate tunisian holidays 2025', function () {
 
     expect(formatDates($holidays))->toMatchSnapshot();
 });
+
+//it('can calculate tunisian holidays list', function () {
+//    $result = [];
+//    for ($i = 1970; $i <= 2037; $i++) {
+//        CarbonImmutable::setTestNowAndTimezone("$i-01-01");
+//        $holidays = Holidays::for(country: 'tn')->get();
+//        $result[$i] = formatDates($holidays);
+//    }
+//    file_put_contents('hijri.json', json_encode($result));
+//    //file_put_contents('hijri.bin', serialize($result));
+//});
