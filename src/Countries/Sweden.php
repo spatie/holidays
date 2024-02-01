6c140a5336c51f4162cb93c764c4168df452f369
Andréas Lundgren <1066486+adevade@users.noreply.github.com>
2024-02-01 10:39:09 +0100

    Add support for Swedish holidays (#199)
    * Add support for Swedish holidays
    
    * Remove timezones

---
+++ src/Countries/Sweden.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Sweden extends Country
+{
+    public function countryCode(): string
+    {
+        return 'se';
+    }
+
+    protected function allHolidays(int $year): array
+    {
+        return array_merge([
+            'Nyårsdagen' => '01-01',
+            'Trettondedag jul' => '01-06',
+            'Första maj' => '05-1',
+            'Nationaldagen' => '06-6',
+            'Juldagen' => '12-25',
+            'Annandag jul' => '12-26',
+        ], $this->variableHolidays($year));
+    }
+
+    /** @return array<string, CarbonImmutable> */
+    protected function variableHolidays(int $year): array
+    {
+        $easter = $this->easter($year);
+
+        /** @var CarbonImmutable $midsummerDay */
+        $midsummerDay = CarbonImmutable::createFromFormat('Y-m-d', "{$year}-06-20");
+
+        if (! $midsummerDay->isSaturday()) {
+            $midsummerDay = $midsummerDay->next(CarbonImmutable::SATURDAY);
+        }
+
+        /** @var CarbonImmutable $halloween */
+        $halloween = CarbonImmutable::createFromFormat('Y-m-d', "{$year}-10-31");
+
+        if (! $halloween->isSaturday()) {
+            $halloween = $halloween->next(CarbonImmutable::SATURDAY);
+        }
+
+        return [
+            'Långfredagen' => $easter->subDays(2),
+            'Påskdagen' => $easter,
+            'Annandag påsk' => $easter->addDay(),
+            'Kristi himmelsfärdsdag' => $easter->addDays(39),
+            'Pingstdagen' => $easter->addDays(49),
+            'Midsommardagen' => $midsummerDay->day > 26
+                ? $midsummerDay->subWeek()
+                : $midsummerDay,
+            'Alla helgons dag' => $halloween,
+        ];
+    }
+}
