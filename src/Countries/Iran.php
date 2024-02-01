d99af11b2b3a1fe59fa4b009e33cceaa0e8c3b82
Ali Salehi <111766206+alisalehi1380@users.noreply.github.com>
2024-02-01 13:23:27 +0330

    Add National and Revolutionary Iran Holidays (#146)
    * Add Iran Holidays
    
    * Add(lang): Iran Holidays
    
    * Fix(Countries): remove `variableHolidays` func | rename Iran Holidays to persian
    
    * Add(test): can calculate iran holidays in local | Change setTestNowAndTimezone to setTestNow

---
+++ src/Countries/Iran.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Spatie\Holidays\Countries;
+
+use Carbon\CarbonImmutable;
+
+class Iran extends Country
+{
+    public function countryCode(): string
+    {
+        return 'ir';
+    }
+    
+    protected function allHolidays(int $year): array
+    {
+        return [
+            'پیروزی انقلاب اسلامی پنجاه و هفت' => '02-11',
+            'روز ملی شدن صنعت نفت' => '03-19',
+            'نخستین روز نوروز' => '03-20',
+            'دومین روز نوروز' => '03-21',
+            'سومین روز نوروز' => '03-22',
+            'چهارمین روز نوروز' => '03-23',
+            'روز جمهوری اسلامی' => '03-31',
+            'سیزده بدر' => '04-01',
+            'رحلت روح‌الله خمینی' => '06-03',
+            'قیام ۱۵ خرداد' => '06-04',
+        ];
+    }
+}
