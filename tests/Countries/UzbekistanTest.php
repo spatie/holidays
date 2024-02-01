1fe6a14faa838a0e7498ad467cff5b1c93615320
shoxrux1996 <shohrux.shomaxmudov@gmail.com>
2024-02-01 15:06:50 +0500

    tests updated and verified

--- tests/Countries/UzbekistanTest.php
+++ tests/Countries/UzbekistanTest.php
@@ -15,28 +15,4 @@ it('can calculate uzbekistan holidays', function ($year) {
         ->not()->toBeEmpty();
 
     expect(formatDates($holidays))->toMatchSnapshot();
-})->with([2000, 2006, 2023, 2024, 2033]);
-
-it('can calculate uzbekistan holidays in English', function () {
-    CarbonImmutable::setTestNow('2024-01-01');
-
-    $holidays = Holidays::for(country: 'uz', locale: 'en')->get();
-
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
-
-    expect(formatDates($holidays))->toMatchSnapshot();
-});
-
-it('can calculate uzbekistan holidays in Russian', function () {
-    CarbonImmutable::setTestNow('2024-01-01');
-
-    $holidays = Holidays::for(country: 'uz', locale: 'ru')->get();
-
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
-
-    expect(formatDates($holidays))->toMatchSnapshot();
-});
+})->with([2000, 2006, 2022, 2023]);
