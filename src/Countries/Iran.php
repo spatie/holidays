e4d2ecdedad62ba158459edde019dcc2ecd0f482
Ali Salehi <alisalehi13800930@gmail.com>
2024-02-01 12:58:52 +0330

    Fix(Countries): remove `variableHolidays` func | rename Iran Holidays to persian

--- src/Countries/Iran.php
+++ src/Countries/Iran.php
@@ -13,28 +13,17 @@ class Iran extends Country
     
     protected function allHolidays(int $year): array
     {
-        return array_merge([
-            'Islamic Revolution Day' => '02-11',
-            'Nationalization of Oil Industry Day' => '03-19',
-            'Nowruz First Day' => '03-20',
-            'Nowruz Second Day' => '03-21',
-            'Nowruz Third Day' => '03-22',
-            'Nowruz Fourth Day' => '03-23',
-            'Islamic Republic Day' => '03-31',
-            'Nature Day' => '04-01',
-            'Death of Khomeini Day' => '06-03',
-            'Revolt of Khordad 15 Day' => '06-04',
-        ], $this->variableHolidays($year));
-    }
-    
-    /** @return array<string, CarbonImmutable> */
-    protected function variableHolidays(int $year): array
-    {
-        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
-            ->setTimezone('Asia/Tehran');
-        
         return [
-            //add more holidays
+            'پیروزی انقلاب اسلامی پنجاه و هفت' => '02-11',
+            'روز ملی شدن صنعت نفت' => '03-19',
+            'نخستین روز نوروز' => '03-20',
+            'دومین روز نوروز' => '03-21',
+            'سومین روز نوروز' => '03-22',
+            'چهارمین روز نوروز' => '03-23',
+            'روز جمهوری اسلامی' => '03-31',
+            'سیزده بدر' => '04-01',
+            'رحلت روح‌الله خمینی' => '06-03',
+            'قیام ۱۵ خرداد' => '06-04',
         ];
     }
 }
