0473c49bf3e2fc38ba2debf1ec595abfdd5898eb
Massoud114 <massoudfataou@gmail.com>
2024-02-01 16:10:00 +0100

    Fix holidays name

--- src/Countries/Benin.php
+++ src/Countries/Benin.php
@@ -17,10 +17,10 @@ class Benin extends Country
     protected function allHolidays(int $year): array
     {
         return array_merge([
-            'Fête du Nouvel An' => '01-01',
-            'Fête annuelle des religions traditionnelles' => '01-10',
-            'Fête du travail' => '05-01',
-            'Fête de l\'indépendance' => '08-01',
+            'Fete du Nouvel An' => '01-01',
+            'Fete annuelle des religions traditionnelles' => '01-10',
+            'Fete du travail' => '05-01',
+            'Fete de l\'independance' => '08-01',
             'Jour de la Toussaint' => '11-01',
             'Jour de Noël' => '12-25',
             'Jour de l\'assomption' => '08-15',
@@ -30,8 +30,7 @@ class Benin extends Country
     /** @return array<string, CarbonImmutable> */
     protected function variableHolidays(int $year): array
     {
-        $easter = CarbonImmutable::createFromTimestamp(easter_date($year))
-            ->setTimezone('Africa/Porto-Novo');
+        $easter = $this->easter($year)->setTimezone('Africa/Porto-Novo');
 
         $hijriYear = Hijri::convertToHijri($easter->format('Y-m-d'))->year;
 
@@ -43,8 +42,8 @@ class Benin extends Country
              * Islamic holidays are based on the Hijri calendar and vary by a day or so.
              */
             'Jour du Maouloud' => Hijri::convertToGregorian(12, 3, $hijriYear)->toImmutable(),
-            'Fête du Ramadan' => Hijri::convertToGregorian(10, 1, $hijriYear)->toImmutable(),
-            'Fête de la Tabaski' => Hijri::convertToGregorian(12, 10, $hijriYear)->toImmutable(),
+            'Fete du Ramadan' => Hijri::convertToGregorian(10, 1, $hijriYear)->toImmutable(),
+            'Fete de la Tabaski' => Hijri::convertToGregorian(12, 10, $hijriYear)->toImmutable(),
         ];
     }
 }
