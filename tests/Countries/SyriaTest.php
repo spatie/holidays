d3d43ed6960571978b2c43617f840600c043bd0e
Amr Alsaleh <amralsaleeh@outlook.com>
2024-02-01 15:44:53 +0300

    Implement islamic holidays

--- tests/Countries/SyriaTest.php
+++ tests/Countries/SyriaTest.php
@@ -5,8 +5,8 @@ namespace Spatie\Holidays\Tests\Countries;
 use Carbon\CarbonImmutable;
 use Spatie\Holidays\Holidays;
 
-it('can calculate syrian holidays', function () {
-    CarbonImmutable::setTestNowAndTimezone('2024-01-01');
+it('can calculate syrian holidays', function ($year) {
+    CarbonImmutable::setTestNow($year.'-01-01');
 
     $holidays = Holidays::for(country: 'sy')->get();
 
@@ -15,4 +15,4 @@ it('can calculate syrian holidays', function () {
         ->not()->toBeEmpty();
 
     expect(formatDates($holidays))->toMatchSnapshot();
-});
+})->with([1970, 1973, 1974, 1975, 1999, 2000, 2001, 2005, 2006, 2007, 2008, 2009, 2016, 2017, 2021, 2022, 2023, 2024, 2025, 2032, 2033, 2034, 2037]);
