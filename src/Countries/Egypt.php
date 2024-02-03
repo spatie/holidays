b87ec3fd475c51dab2ae634b142907965493cec5
Wessam Ahmed <wessam@cobbleweb.co.uk>
2024-02-03 18:05:08 +0200

    Add holidays statically/Turkey solution

--- src/Countries/Egypt.php
+++ src/Countries/Egypt.php
@@ -4,10 +4,226 @@ namespace Spatie\Holidays\Countries;
 
 use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
-use GeniusTS\HijriDate\Hijri;
+use RuntimeException;
 
 class Egypt extends Country
 {
+    protected const EID_AL_FITR_HOLIDAYS = [
+        2005 => '11-04',
+        2006 => '10-24',
+        2007 => '10-13',
+        2008 => '10-02',
+        2009 => '09-21',
+        2010 => '09-10',
+        2011 => '08-30',
+        2012 => '08-19',
+        2013 => '08-08',
+        2014 => '07-28',
+        2015 => '07-17',
+        2016 => '07-07',
+        2017 => '06-26',
+        2018 => '06-15',
+        2019 => '06-05',
+        2020 => '05-24',
+        2021 => '05-13',
+        2022 => '05-02',
+        2023 => '04-20',
+        2024 => '04-10',
+        2025 => '03-31',
+        2026 => '03-21',
+        2027 => '03-10',
+        2028 => '02-27',
+        2029 => '02-15',
+        2030 => '02-04',
+        2031 => '01-25',
+        2032 => '01-15',
+        2033 => '01-03',
+        2034 => '12-13',
+        2035 => '12-02',
+        2036 => '11-20',
+        2037 => '11-09',
+    ];
+
+    protected const ARAFAT_DAY_HOLIDAYS = [
+        2005 => '01-21',
+        2006 => '01-10',
+        2007 => '01-01',
+        2008 => '12-09',
+        2009 => '11-26',
+        2010 => '11-15',
+        2011 => '11-05',
+        2012 => '10-25',
+        2013 => '10-15',
+        2014 => '10-04',
+        2015 => '09-23',
+        2016 => '09-11',
+        2017 => '08-31',
+        2018 => '08-20',
+        2019 => '08-10',
+        2020 => '07-30',
+        2021 => '07-19',
+        2022 => '07-09',
+        2023 => '06-27',
+        2024 => '06-16',
+        2025 => '06-06',
+        2026 => '05-26',
+        2027 => '05-16',
+        2028 => '05-05',
+        2029 => '04-24',
+        2030 => '04-13',
+        2031 => '04-02',
+        2032 => '03-21',
+        2033 => '03-11',
+        2034 => '03-01',
+        2035 => '02-18',
+        2036 => '02-07',
+        2037 => '01-26',
+    ];
+
+    protected const EID_AL_ADHA_HOLIDAYS = [
+        2005 => '01-22',
+        2006 => '01-11',
+        2007 => '01-02',
+        2008 => '12-10',
+        2009 => '11-27',
+        2010 => '11-16',
+        2011 => '11-06',
+        2012 => '10-26',
+        2013 => '10-16',
+        2014 => '10-05',
+        2015 => '09-24',
+        2016 => '09-12',
+        2017 => '08-31',
+        2018 => '08-21',
+        2019 => '08-11',
+        2020 => '07-31',
+        2021 => '07-20',
+        2022 => '07-09',
+        2023 => '06-28',
+        2024 => '06-17',
+        2025 => '06-07',
+        2026 => '05-27',
+        2027 => '05-17',
+        2028 => '05-06',
+        2029 => '04-25',
+        2030 => '04-14',
+        2031 => '04-03',
+        2032 => '03-22',
+        2033 => '03-12',
+        2034 => '03-02',
+        2035 => '02-19',
+        2036 => '02-08',
+        2037 => '01-27',
+    ];
+
+    protected const ISLAMIC_NEW_YEAR_HOLIDAYS = [
+        2005 => '02-10',
+        2006 => '01-31',
+        2007 => '01-20',
+        2008 => '01-10',
+        2009 => '12-18',
+        2010 => '12-07',
+        2011 => '11-27',
+        2012 => '11-15',
+        2013 => '11-05',
+        2014 => '10-25',
+        2015 => '10-14',
+        2016 => '10-03',
+        2017 => '09-22',
+        2018 => '09-11',
+        2019 => '08-31',
+        2020 => '08-20',
+        2021 => '08-09',
+        2022 => '07-30',
+        2023 => '07-19',
+        2024 => '07-08',
+        2025 => '06-06',
+        2026 => '06-17',
+        2027 => '06-07',
+        2028 => '05-26',
+        2029 => '05-15',
+        2030 => '05-05',
+        2031 => '04-24',
+        2032 => '04-12',
+        2033 => '04-01',
+        2034 => '03-22',
+        2035 => '03-12',
+        2036 => '02-29',
+        2037 => '02-17',
+    ];
+
+    protected const ASHURA_HOLIDAYS = [
+        2005 => '02-19',
+        2006 => '02-09',
+        2007 => '01-29',
+        2008 => '01-19',
+        2009 => '12-27',
+        2010 => '12-16',
+        2011 => '12-06',
+        2012 => '11-25',
+        2013 => '11-15',
+        2014 => '11-04',
+        2015 => '10-24',
+        2016 => '10-13',
+        2017 => '10-02',
+        2018 => '09-21',
+        2019 => '09-10',
+        2020 => '08-30',
+        2021 => '08-19',
+        2022 => '08-08',
+        2023 => '07-28',
+        2024 => '07-17',
+        2025 => '07-07',
+        2026 => '06-26',
+        2027 => '06-15',
+        2028 => '06-04',
+        2029 => '05-24',
+        2030 => '05-13',
+        2031 => '05-02',
+        2032 => '04-20',
+        2033 => '04-10',
+        2034 => '03-30',
+        2035 => '03-19',
+        2036 => '03-08',
+        2037 => '02-25',
+    ];
+
+    protected const PROPHET_MUHAMMAD_BIRTHDAY_HOLIDAYS = [
+        2005 => '04-21',
+        2006 => '04-11',
+        2007 => '03-31',
+        2008 => '03-20',
+        2009 => '03-09',
+        2010 => '02-26',
+        2011 => '02-16',
+        2012 => '02-05',
+        2013 => '01-24',
+        2014 => '01-13',
+        2015 => '12-23',
+        2016 => '12-12',
+        2017 => '12-01',
+        2018 => '11-20',
+        2019 => '11-09',
+        2020 => '10-29',
+        2021 => '10-21',
+        2022 => '10-08',
+        2023 => '09-28',
+        2024 => '09-16',
+        2025 => '09-06',
+        2026 => '08-26',
+        2027 => '08-15',
+        2028 => '08-04',
+        2029 => '07-25',
+        2030 => '07-14',
+        2031 => '07-03',
+        2032 => '06-21',
+        2033 => '06-10',
+        2034 => '05-31',
+        2035 => '05-21',
+        2036 => '05-09',
+        2037 => '04-29',
+    ];
+
     public function countryCode(): string
     {
         return 'eg';
@@ -40,25 +256,59 @@ class Egypt extends Country
     {
         $orthodoxEaster = $this->orthodoxEaster($year);
 
-        $hijriHolidays = [
-            'Islamic New Year' => '01-01',
-            'Ashura' => '01-10',
-            'Birthday of the Prophet Muhammad' => '03-12',
-            'Eid al-Fitr' => '10-01',
-            'Eid al-Fitr Day 2' => '10-02',
-            'Eid al-Fitr Day 3' => '10-03',
-            'Arafat Day' => '12-09',
-            'Eid al-Adha' => '12-10',
-            'Eid al-Adha Day 2' => '12-11',
-            'Eid al-Adha Day 3' => '12-12',
-            'Eid al-Adha Day 4' => '12-13',
-        ];
+        $eidAlFitrDates = $this->getIslamicHolidayDatesForYear(self::EID_AL_FITR_HOLIDAYS, $year, 'Eid al-Fitr', 3);
+        $eidAlAdhaDates = $this->getIslamicHolidayDatesForYear(self::EID_AL_ADHA_HOLIDAYS, $year, 'Eid al-Adha', 4);
+        $arafatDayDates = $this->getIslamicHolidayDatesForYear(self::ARAFAT_DAY_HOLIDAYS, $year, 'Arafat Day');
+        $islamicNewYearDates = $this->getIslamicHolidayDatesForYear(self::ISLAMIC_NEW_YEAR_HOLIDAYS, $year, 'Islamic New Year');
+        $ashuraDates = $this->getIslamicHolidayDatesForYear(self::ASHURA_HOLIDAYS, $year, 'Ashura');
+        $prophetMuhammadBirthdayDates = $this->getIslamicHolidayDatesForYear(self::PROPHET_MUHAMMAD_BIRTHDAY_HOLIDAYS, $year, 'Birthday of the Prophet Muhammad');
 
         return array_merge([
             'Coptic Good Friday' => $orthodoxEaster->subDays(2)->toImmutable(),
             'Coptic Holy Saturday' => $orthodoxEaster->subDays()->toImmutable(),
             'Coptic Easter Sunday' => $orthodoxEaster->toImmutable(),
-        ], $this->convertHijriToGregorianHolidays($hijriHolidays, $year));
+        ], $eidAlFitrDates, $eidAlAdhaDates, $arafatDayDates, $islamicNewYearDates, $ashuraDates, $prophetMuhammadBirthdayDates);
+    }
+
+    /**
+     * Prepare holiday dates for the given year.
+     *
+     * @param  array<int, string>  $holidayDates  Array mapping years to dates.
+     * @param  int  $year  The year for which to prepare holiday dates.
+     * @param  string  $holidayName  The name of the holiday.
+     * @param  int  $duration  The duration of the holiday in days.
+     * @return array<string, CarbonImmutable|false> An array of holiday dates.
+     */
+    private function getIslamicHolidayDatesForYear(array $holidayDates, int $year, string $holidayName, int $duration = 1): array
+    {
+        $dates = [];
+
+        /**
+         * No reliable sources exist for Islamic holidays observed in Egypt prior to 2005.
+         * So we'll only calculate holidays from 2005 onwards.
+         *
+         * @see https://www.timeanddate.com/holidays/egypt
+         */
+        if (isset($holidayDates[$year])) {
+            $startDay = CarbonImmutable::createFromFormat('Y-m-d', sprintf('%s-%s', $year, $holidayDates[$year]));
+
+            if (! $startDay instanceof CarbonImmutable) {
+                throw new RuntimeException('Date could not be created.');
+            }
+
+            if ($duration === 1) {
+                // For single-day holidays, use the holiday name without "Day"
+                $dates[$holidayName] = $startDay;
+            } else {
+                // For multi-day holidays, append "Day N" for the second day onwards
+                for ($i = 0; $i < $duration; $i++) {
+                    $dayLabel = $i === 0 ? $holidayName : sprintf('%s Day %d', $holidayName, $i + 1);
+                    $dates[$dayLabel] = $startDay->addDays($i);
+                }
+            }
+        }
+
+        return $dates;
     }
 
     /**
@@ -82,63 +332,6 @@ class Egypt extends Country
         }
 
         return $holidays;
-    }
-
-    /**
-     * @param  array<string, string>  $hijriHolidays
-     * @return array<string, CarbonImmutable>
-     */
-    private function convertHijriToGregorianHolidays(array $hijriHolidays, int $year): array
-    {
-        /**
-         * ---------------------------------------------------------------
-         * Code Adaptation
-         * ---------------------------------------------------------------
-         * This strategy was adapted from PR #56.
-         * Original contribution by: YazidKHALDI
-         * See: https://github.com/spatie/holidays/pull/56/
-         * ---------------------------------------------------------------
-         */
-        Hijri::setDefaultAdjustment(-1);
-
-        $currentHijriYear = (int) Hijri::convertToHijri($year.'-01-01')->format('Y');
-        $gregorianHolidays = [];
-
-        foreach ($hijriHolidays as $holidayName => $hijriDate) {
-            $gregorianDate = $this->getGregorianDateForHijriHoliday($hijriDate, $currentHijriYear, $year);
-            if (! is_null($gregorianDate)) {
-                $gregorianHolidays[$holidayName] = $gregorianDate;
-            }
-        }
-
-        return $gregorianHolidays;
-    }
-
-    private function getGregorianDateForHijriHoliday(string $hijriDate, int $hijriYear, int $gregorianYear): ?CarbonImmutable
-    {
-        [$month, $day] = explode('-', $hijriDate);
-
-        /**
-         * Converts a Hijri date to a Gregorian date and checks its alignment with the Gregorian year.
-         * Since a Hijri year is about 11 days shorter than a Gregorian year, we're checking
-         * if the converted date falls within the specified Gregorian year.
-         */
-        $currentYearDate = Hijri::convertToGregorian((int) $day, (int) $month, $hijriYear);
-        if ($currentYearDate->format('Y') == $gregorianYear) {
-            return $currentYearDate->toImmutable();
-        }
-
-        /**
-         * If it does not fall within the specified Gregorian year, we're checking if the converted date
-         * falls within the next Gregorian year. This is to account for the fact that the Hijri year
-         * may start in the previous (that is to say, currently specified) Gregorian year.
-         */
-        $nextYearDate = Hijri::convertToGregorian((int) $day, (int) $month, $hijriYear + 1);
-        if ($nextYearDate->format('Y') == $gregorianYear) {
-            return $nextYearDate->toImmutable();
-        }
-
-        return null;
     }
 
     /**
