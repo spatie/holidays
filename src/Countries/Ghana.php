0ab3409bbf7648de1d9719ff02bf59417c8529f2
Victor Ikpeba <ikpeba4real@gmail.com>
2024-02-08 09:56:10 +0000

    Enhanced holiday dates in Ghana (#80)
    * Added holidays for `Ghana`
    
    * fix timezone when creating farmers day
    
    * Updated holidays that falls within weekend to be observed on the next monday
    
    * Fix return types
    
    * pr suggestions implemented
    
    * removed unwanted timezone in params
    
    * removed timezone
    
    * Updated test
    
    * removed test files
    
    * Add back test files
    
    * Added return types to methods

--- src/Countries/Ghana.php
+++ src/Countries/Ghana.php
@@ -11,18 +11,81 @@ class Ghana extends Country
         return 'gh';
     }
 
+    /**
+     * Return carbon date for christmas
+     *
+     * @return CarbonImmutable
+     */
+    protected function getChristmasDay(int $year)
+    {
+        return new CarbonImmutable($year . '-12-25');
+    }
+
+    /**  @return  CarbonImmutable */
+    protected function christmasDay(int $year): CarbonImmutable
+    {
+        $christmasDay = $this->getChristmasDay($year);
+
+        if ($christmasDay->isSaturday()) {
+
+            $christmasDay = $christmasDay->next('monday');
+        }
+
+        if ($christmasDay->isSunday()) {
+            $christmasDay = $christmasDay->next('tuesday');
+        }
+
+        return $christmasDay;
+    }
+
+    /**  @return  CarbonImmutable */
+    protected function boxingDay(int $year): CarbonImmutable
+    {
+        $christmasDay = $this->getChristmasDay($year);
+        $boxingDay = new CarbonImmutable($year . '-12-26');
+
+        if ($christmasDay->isFriday()) {
+            $boxingDay = $boxingDay->next('monday');
+        }
+        if ($christmasDay->isSaturday()) {
+            $boxingDay = $boxingDay->next('tuesday');
+        }
+
+        return $boxingDay;
+    }
+
+    /**
+     * Get holiday
+     *
+     * For example: If a holiday falls on a weekend, the new day to be observed is the next monday
+     * @return  CarbonImmutable
+     */
+    protected function getHoliday(int $year, string $monthAndDay): CarbonImmutable
+    {
+        $newYearsDay = new CarbonImmutable($year . '-' . $monthAndDay);
+
+        if ($newYearsDay->isWeekend()) {
+            $newYearsDay = $newYearsDay->next('monday');
+        }
+
+        return $newYearsDay;
+    }
+
     protected function allHolidays(int $year): array
     {
-        return array_merge([
-            'New Year\'s Day' => '01-01',
-            'Constitution Day' => '01-07',
-            'Independence Day' => '03-06',
-            'May Day' => '05-01',
-            'Founder\'s Day' => '08-04',
-            'Kwame Nkrumah Memorial Day' => '09-21',
-            'Christmas Day' => '12-25',
-            'Boxing Day' => '12-26',
-        ], $this->variableHolidays($year));
+        return array_merge(
+            [
+                'New Year\'s Day' => $this->getHoliday($year, '01-01'),
+                'Constitution Day' => $this->getHoliday($year, '01-07'),
+                'Independence Day' => $this->getHoliday($year, '03-06'),
+                'May Day' => $this->getHoliday($year, '05-01'),
+                'Founder\'s Day' => $this->getHoliday($year, '08-04'),
+                'Kwame Nkrumah Memorial Day' => $this->getHoliday($year, '09-21'),
+                'Christmas Day' => $this->christmasDay($year),
+                'Boxing Day' => $this->boxingDay($year),
+            ],
+            $this->variableHolidays($year)
+        );
     }
 
     /** @return array<string, CarbonImmutable> */
@@ -30,9 +93,17 @@ class Ghana extends Country
     {
         $easter = $this->easter($year);
 
+        $farmersDay = new CarbonImmutable('first friday of December ' . $year);
+
         return [
+            'Farmers Day' => $farmersDay,
             'Good Friday' => $easter->subDays(2),
             'Easter Monday' => $easter->addDay(),
+
+            // NB: *** There are no fixed dates for the Eid-Ul-Fitr and Eid-Ul-Adha because they are movable feasts.
+            // The dates for their observation are provided by the Office of the Chief Imam in the course of the year.
+            // 'Eid-Ul-Fitr' => "",
+            // 'Eid-Ul-Adha' => "",
         ];
     }
 }
