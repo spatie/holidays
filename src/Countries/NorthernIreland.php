8e8a4a15e30ab0e14119b2010dc457294f52796a
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-02-09 11:55:53 +0100

    cleanup Wales related countries

--- src/Countries/NorthernIreland.php
+++ src/Countries/NorthernIreland.php
@@ -14,62 +14,38 @@ class NorthernIreland extends Wales
 
     protected function allHolidays(int $year): array
     {
-        $regularHolidays = array_merge(
-            $this->newYearsDay($year),
-            $this->stPatricksDay($year),
+        return array_merge(
+            $this->observedHolidays($year),
             $this->earlyMayBankHoliday($year),
-            $this->battleOfTheBoyne($year),
-            [
-                'Spring bank holiday' => 'last monday of may',
-                'Summer bank holiday' => 'last monday of august',
-            ],
-            $this->christmasDay($year),
-            $this->boxingDay($year),
-            $this->variableHolidays($year)
+            $this->variableHolidays($year),
+            $this->oneOffHolidays($year),
         );
-
-        $oneOffHolidays = $this->oneOffHolidays($year);
-
-        return array_merge($regularHolidays, $oneOffHolidays);
     }
 
-    /** @return array<string, CarbonInterface> */
-    private function stPatricksDay(int $year): array
+    /** @return array<string, string|CarbonInterface> */
+    protected function observedHolidays(int $year): array
     {
-        $stPatricksDay = new CarbonImmutable($year.'-03-17', 'Europe/London');
-        $key = 'St Patrick\'s Day';
+        $holidays = [
+            'New Year\'s Day' => '01-01',
+            'St Patrick\'s Day' => '03-17',
+            'Battle of the Boyne (Orangemen\'s Day)' => '07-12',
+            'Christmas Day' => '12-25',
+            'Boxing Day' => '12-26',
+        ];
 
-        if ($stPatricksDay->isWeekend()) {
-            $key .= ' (substitute day)';
-            $stPatricksDay = $stPatricksDay->next('monday');
+        foreach ($holidays as $name => $date) {
+            $observedDay = match ($name) {
+                'Christmas Day' => $this->observedChristmasDay($year),
+                'Boxing Day' => $this->observedBoxingDay($year),
+                default => $this->weekendToNextMonday($date, $year),
+            };
+
+            if ($observedDay) {
+                $holidays[$name.' (substitute day)'] = $observedDay;
+                unset($holidays[$name]);
+            }
         }
 
-        return [$key => $stPatricksDay];
-    }
-
-    /** @return array<string, CarbonInterface> */
-    private function battleOfTheBoyne(int $year): array
-    {
-        $battleOfTheBoyne = new CarbonImmutable($year.'-07-12', 'Europe/London');
-        $key = 'Battle of the Boyne (Orangemen\'s Day)';
-
-        if ($battleOfTheBoyne->isWeekend()) {
-            $key .= ' (substitute day)';
-            $battleOfTheBoyne = $battleOfTheBoyne->next('monday');
-        }
-
-        return [$key => $battleOfTheBoyne];
-    }
-
-    /** @return array<string, CarbonImmutable> */
-    protected function oneOffHolidays(int $year): array
-    {
-        return match ($year) {
-            2022 => [
-                'Platinum Jubilee bank holiday' => new CarbonImmutable('2022-06-03', 'Europe/London'),
-                'Bank Holiday for the State Funeral of Queen Elizabeth II' => new CarbonImmutable('2022-09-19', 'Europe/London'),
-            ],
-            default => [],
-        };
+        return $holidays;
     }
 }
