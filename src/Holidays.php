26644a65f2f9b80546e323fee7fb2b06bbc11fbe
Petr Katerinak <33215381+inDeev@users.noreply.github.com>
2024-06-24 09:48:22 +0200

    Add getInRange function (#150)
    * add getInRange function
    
    * Update Holidays.php
    
    * Update Holidays.php
    
    ---------
    
    Co-authored-by: Petr Katerinak <katerinak@indeev.eu>

--- src/Holidays.php
+++ src/Holidays.php
@@ -15,6 +15,8 @@ class Holidays
         protected Country $country,
         protected int $year,
         protected ?string $locale = null,
+        protected ?CarbonImmutable $from = null,
+        protected ?CarbonImmutable $to = null,
     ) {
     }
 
@@ -43,6 +45,43 @@ class Holidays
         return static::for($country, $year, $this->locale)
             ->calculate()
             ->toArray();
+    }
+
+    /**
+     * getInRange method allows you to pick holidays in a range of dates,
+     *   - dates are inclusive.
+     *   - dates are swappable, lower date could be passed as second argument.
+     *   - dates could be a CarbonInterface or a string.
+     *   - acceptable strings formats are 'Y-m-d' or 'Y-m' or 'Y'
+     *   - if passed string is 'Y-m' or 'Y' it will be converted to first(from) / last{to} day of the month(from) / year(to)
+     * E.g. to retrieve all holidays in between
+     *    - 2020-01-01 and 2024-12-31, you could use: getInRange('2020-01-01', '2024-12-31'), getInRange('2020-01', '2024-12') or getInRange('2020', '2024')
+     *    - 2024-06-01 and 2025-05-30, you could use: getInRange('2024-06-01', '2025-05-30'), getInRange('2024-06', '2025-05')
+     * @return array<string, string> date => name
+     */
+    public function getInRange(CarbonInterface|string $from, CarbonInterface|string $to): array
+    {
+        if (! $from instanceof CarbonImmutable) {
+            $from = match (strlen($from)) {
+                4 => CarbonImmutable::parse($from . '-01-01'),
+                7 => CarbonImmutable::parse($from . '-01'),
+                default => CarbonImmutable::parse($from),
+            };
+        }
+
+        if (! $to instanceof CarbonImmutable) {
+            $to = match (strlen($to)) {
+                4 => CarbonImmutable::parse($to . '-12-31'),
+                7 => CarbonImmutable::parse($to)->endOfMonth(),
+                default => CarbonImmutable::parse($to),
+            };
+        }
+
+        if ($from->gt($to)) {
+            [$from, $to] = [$to, $from];
+        }
+
+        return $this->country->getInRange($from, $to);
     }
 
     public function isHoliday(CarbonInterface|string $date, Country|string|null $country = null): bool
