ad8abf91325bb4d9cfd27ddeb2d602fd1f885a95
Victor Allen <vamuigua@gmail.com>
2024-10-10 13:16:53 +0300

    update(holiday): Utamaduni day renamed to Mazingira day (#245)
    * update(holiday): Utamaduni day renamed to Mazingira day
    
    * Update holiday names to match the Public Holidays Act in Kenya
    
    * feat: Add dynamic October 10th holiday naming for Kenya
    
    - Introduced logic to dynamically calculate the name of the October 10th holiday based on the year:
      - Mazingira Day for 2024 and beyond
      - Utamaduni Day from 2022-2023
      - Huduma Day from 2019-2021
      - Moi Day for 2018 and earlier
    
    - Updated test cases to validate holiday names across different years.

--- tests/Countries/KenyaTest.php
+++ tests/Countries/KenyaTest.php
@@ -16,3 +16,63 @@ it('can calculate kenyan holidays', function () {
 
     expect(formatDates($holidays))->toMatchSnapshot();
 });
+
+it('calculates October 10th holiday as Mazingira Day in 2024', function () {
+    CarbonImmutable::setTestNow('2024-10-10');
+
+    $holidays = Holidays::for(country: 'ke')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(findDate($holidays, 'Mazingira Day'))
+        ->toEqual(CarbonImmutable::create(2024, 10, 10));
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('calculates October 10th holiday as Utamaduni Day in 2023', function () {
+    CarbonImmutable::setTestNow('2023-10-10');
+
+    $holidays = Holidays::for(country: 'ke')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(findDate($holidays, 'Utamaduni Day'))
+        ->toEqual(CarbonImmutable::create(2023, 10, 10));
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('calculates October 10th holiday as Huduma Day in 2021', function () {
+    CarbonImmutable::setTestNow('2021-10-10');
+
+    $holidays = Holidays::for(country: 'ke')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(findDate($holidays, 'Huduma Day'))
+        ->toEqual(CarbonImmutable::create(2021, 10, 10));
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('calculates October 10th holiday as Moi Day before 2019', function () {
+    CarbonImmutable::setTestNow('2018-10-10');
+
+    $holidays = Holidays::for(country: 'ke')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(findDate($holidays, 'Moi Day'))
+        ->toEqual(CarbonImmutable::create(2018, 10, 10));
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
