d5907070a648f3a1d135a13fb8181e9e1e0a874f
Pau Rodellino <pau@creagia.com>
2024-12-27 14:34:30 +0100

    test with snapshots
    removed generic snapshot and separate them for every year just in case somebody still wants to check holidays until 2022

--- tests/Countries/SpainTest.php
+++ tests/Countries/SpainTest.php
@@ -6,26 +6,118 @@ use Carbon\CarbonImmutable;
 use Spatie\Holidays\Countries\Spain;
 use Spatie\Holidays\Holidays;
 
-it('can calculate spanish holidays', function () {
+// 2025
+it('can calculate spanish holidays 2025', function () {
     CarbonImmutable::setTestNow('2025-01-01');
 
     $holidays = Holidays::for(country: 'es')->get();
 
     expect($holidays)
         ->toBeArray()
-        ->not()->toBeEmpty();
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
 
-    expect(formatDates($holidays))->toMatchSnapshot();
 });
 
-it('can calculate spanish regional holidays', function () {
+it('can calculate spanish regional holidays 2025', function () {
     CarbonImmutable::setTestNow('2025-01-01');
 
     $holidays = Holidays::for(Spain::make('es-ct'))->get();
 
     expect($holidays)
         ->toBeArray()
-        ->not()->toBeEmpty();
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
 
-    expect(formatDates($holidays))->toMatchSnapshot();
 });
+
+// 2024
+it('can calculate spanish holidays 2024', function () {
+    CarbonImmutable::setTestNow('2024-01-01');
+
+    $holidays = Holidays::for(country: 'es')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+
+});
+
+it('can calculate spanish regional holidays 2024', function () {
+    CarbonImmutable::setTestNow('2024-01-01');
+
+    $holidays = Holidays::for(Spain::make('es-ar'))->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+});
+
+// 2023
+it('can calculate spanish holidays 2023', function () {
+    CarbonImmutable::setTestNow('2023-01-01');
+
+    $holidays = Holidays::for(country: 'es')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+});
+
+it(/**
+ * @throws \JsonException
+ */ 'can calculate spanish regional holidays 2023', function () {
+    CarbonImmutable::setTestNow('2023-01-01');
+
+    $holidays = Holidays::for(Spain::make('es-ar'))->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+
+});
+
+// 2022
+it('can calculate spanish holidays 2022', function () {
+    CarbonImmutable::setTestNow('2022-01-01');
+
+    $holidays = Holidays::for(country: 'es')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+
+});
+
+it('can calculate spanish regional holidays 2022', function () {
+    CarbonImmutable::setTestNow('2022-01-01');
+
+    $holidays = Holidays::for(Spain::make('es-ar'))->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()
+        ->toBeEmpty()
+        ->and(formatDates($holidays))
+        ->toMatchSnapshot();
+});
+
