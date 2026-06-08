00c77cdd35c6108866d4c7761c8e6c466c7f10e4
Luis Parrado <luisprmat@gmail.com>
2026-06-08 06:22:11 -0500

    Add new Colombian national holiday from year 2026

--- tests/Countries/ColombiaTest.php
+++ tests/Countries/ColombiaTest.php
@@ -16,3 +16,25 @@ it('can calculate colombian holidays', function () {
 
     expect(formatDates($holidays))->toMatchSnapshot();
 });
+
+it('can calculate colombian holidays from 2026', function () {
+    CarbonImmutable::setTestNow('2026-01-01');
+
+    $holidays = Holidays::for(country: 'co')->get();
+
+    expect($holidays)
+        ->toBeArray()
+        ->not()->toBeEmpty();
+
+    expect(formatDates($holidays))->toMatchSnapshot();
+});
+
+it('takes into account a new holiday only from year 2026', function () {
+    CarbonImmutable::setTestNow('2026-01-01');
+
+    $holidays2025 = Holidays::for(country: 'co', year: 2025)->get();
+    $holidays2026 = Holidays::for(country: 'co')->get();
+
+    expect(count($holidays2025))->toBe(18);
+    expect(count($holidays2026))->toBe(19);
+});
