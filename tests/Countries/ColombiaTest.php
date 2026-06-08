64182a464023a2314eebe5e3dce92e2c9453e564
Luis Parrado <luisprmat@gmail.com>
2026-06-08 14:23:56 -0500

    Refactor tests

--- tests/Countries/ColombiaTest.php
+++ tests/Countries/ColombiaTest.php
@@ -5,8 +5,8 @@ namespace Spatie\Holidays\Tests\Countries;
 use Carbon\CarbonImmutable;
 use Spatie\Holidays\Holidays;
 
-it('can calculate colombian holidays', function () {
-    CarbonImmutable::setTestNow('2024-01-01');
+it('can calculate colombian holidays', function (int $year, int $totalHolidays) {
+    CarbonImmutable::setTestNow("{$year}-01-01");
 
     $holidays = Holidays::for(country: 'co')->get();
 
@@ -14,27 +14,10 @@ it('can calculate colombian holidays', function () {
         ->toBeArray()
         ->not()->toBeEmpty();
 
-    expect(formatDates($holidays))->toMatchSnapshot();
-});
-
-it('can calculate colombian holidays from 2026', function () {
-    CarbonImmutable::setTestNow('2026-01-01');
-
-    $holidays = Holidays::for(country: 'co')->get();
-
-    expect($holidays)
-        ->toBeArray()
-        ->not()->toBeEmpty();
+    expect(count($holidays))->toBe($totalHolidays);
 
     expect(formatDates($holidays))->toMatchSnapshot();
-});
-
-it('takes into account a new holiday only from year 2026', function () {
-    CarbonImmutable::setTestNow('2026-01-01');
-
-    $holidays2025 = Holidays::for(country: 'co', year: 2025)->get();
-    $holidays2026 = Holidays::for(country: 'co')->get();
-
-    expect(count($holidays2025))->toBe(18);
-    expect(count($holidays2026))->toBe(19);
-});
+})->with([
+    [2025, 18],
+    [2026, 19],
+]);
